a0b47c4ab8e7e6c5624f11692c4fc73e87a0f518
marvel <eternalklaus@naver.com>
2021-01-11 12:49:35 +0900

    Added analysis

--- Leetcode/longest-substring-without-repeating-characters.py
+++ Leetcode/longest-substring-without-repeating-characters.py
@@ -1,13 +1,18 @@
 class Solution(object):
     maxlen = 0
+    memoization = {}
     def lengthOfLongestSubstring(self, s):
+        
         """
         :type s: str
         :rtype: int
         """
         def splitstr(substr): 
             if len(substr) <= self.maxlen:
-                return 0
+                return 0 
+            if substr in self.memoization.keys(): # already calculated
+                return self.memoization[substr]
+
             # TODO: add another constraint in order to fasten proceeding time.
             for i in range(len(substr)):
                 idx = substr.rfind(substr[i]) 
@@ -16,14 +21,23 @@ class Solution(object):
                     substr2 = substr[i:idx]
                     substr3 = substr[i+1:]
                     # print substr1, substr2, substr3
-                    tmp = max(splitstr(substr1), splitstr(substr2), splitstr(substr3))
                     
-                    self.maxlen = tmp if tmp > self.maxlen else self.maxlen
-                    return tmp
+                    max1 = splitstr(substr1)
+                    max2 = splitstr(substr2)
+                    max3 = splitstr(substr3)
+
+                    _maxlen = max(max1, max2, max3)
+                    ### update memoization
+                    self.memoization[substr] = _maxlen 
+                    ### update maxlen
+                    self.maxlen = _maxlen if _maxlen > self.maxlen else self.maxlen
+                    
+                    return _maxlen
 
             return len(substr) # if all chars are the only accurence, return len(substr)
         return splitstr(s)
         
 
 sl = Solution()
-print sl.lengthOfLongestSubstring("ivqpsqbpqjogwnswtimdlbxcwgeaenwokndefetwpjenwwksgwxszuwxb")
+print sl.lengthOfLongestSubstring("abcd")
+# on the server, it returns 0. IDK why.
