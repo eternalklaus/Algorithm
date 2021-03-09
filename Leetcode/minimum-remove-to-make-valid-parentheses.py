5a9452f992a0c48e551324273bcc8aafb069546e
Jiwon Choi <eternalklaus@naver.com>
2021-03-09 18:24:07 +0900

    Passed

--- Leetcode/minimum-remove-to-make-valid-parentheses.py
+++ Leetcode/minimum-remove-to-make-valid-parentheses.py
@@ -1,35 +1,23 @@
 class Solution(object):
-    parentheses = ['(', ')']
-    stack_open = []
-    stack_close = []
     
     def minRemoveToMakeValid(self, s):
+        stack_open = []   # stack for holding indce of '(' to remove
+        stack_close = []  # stack for holding indce of ')' to remove
         i = 0
         while i < len(s):
             if s[i] == '(':
-                self.stack_open.append(i)
-            elif s[i] == '()':
+                stack_open.append(i)
+            elif s[i] == ')': # '(' and ')' matched
                 if len(stack_open) > 0:
-                    self.stack_open.pop()
+                    stack_open.pop()
                 else:
-                    self.stack_close.append(i)
+                    stack_close.append(i) # sole ')'
             i += 1
-
-        print self.stack_open
-        print self.stack_close
+        
         
         ### Remove the charectors indexed by stacks - open, close
-        stack = self.stack_open + self.stack_close
+        stack = stack_open + stack_close
         for i in stack:
             s = s[:i] + '#' + s[i+1:]
-
         return s.replace('#','') 
         
-        
-        """
-        :type s: str
-        :rtype: str
-        """
-
-sl = Solution()
-print sl.minRemoveToMakeValid("(a(b(c)d)")
