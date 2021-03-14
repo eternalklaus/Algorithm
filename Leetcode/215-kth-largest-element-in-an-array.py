260faa9cd14a36f6ca6b0aa14f13b6c0bb2bf216
Jiwon Choi <eternalklaus@naver.com>
2021-03-14 16:03:45 +0900

    Passed

--- Leetcode/215-kth-largest-element-in-an-array.py
+++ Leetcode/215-kth-largest-element-in-an-array.py
@@ -1,26 +1,22 @@
-# Input: nums = [3,2,1,5,6,4], k = 2
-
-
 class Solution(object):
     def findKthLargest(self, nums, k):
         # terminate condition
-        if len(nums) <= 1: 
-            
-        
         pivot = 0
         lnums = []
         rnums = []
-        for i in range(1, len(nums)):
-            if nums[i] < nums[pivot]:
+        for i in range(1, len(nums)): # sort by [max ... min] order
+            if nums[i] >= nums[pivot]:
                 lnums.append(nums[i])
-            elif nums[i] >= nums[pivot]:
+            elif nums[i] < nums[pivot]:
                 rnums.append(nums[i])
         
-        if len(lnums) == k-1:
+        if len(lnums) == k-1: # found exact component!
             return nums[pivot]
-        elif len(lnums) >= k:
-            return self.findKthLargest(lnums, k) # We don't have to sort rnums
-        elif len(lnums) < k:
-            return self.findKthLargest(rnums, k - len(lnums))
+        elif len(lnums) < k: # search again with right list
+            return self.findKthLargest(rnums, k - len(lnums) - 1) # !!! [here] k - len(lnums) incures recursive loop..
+        elif len(lnums) >= k: # search again with left list
+            return self.findKthLargest(lnums, k) 
+
         
-        
+sl = Solution()
+print sl.findKthLargest([7,6,5,4,3,2,1], 2)
