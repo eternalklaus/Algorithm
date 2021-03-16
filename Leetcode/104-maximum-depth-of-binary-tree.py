9c9d25aa33c9d20490328006c8a6ec7fedc531c3
Jiwon Choi <eternalklaus@naver.com>
2021-03-17 06:15:16 +0900

    Added 104

---
+++ Leetcode/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getdepth(self, node):
+        if node == None:
+            return 0 
+        lchild = node.left 
+        rchild = node.right
+        
+        ldepth = 1 + self.getdepth(lchild)
+        rdepth = 1 + self.getdepth(rchild)
+        max(ldepth, rdepth)
+
+    def maxDepth(self, root):
+        return self.getdepth(root)
+        
