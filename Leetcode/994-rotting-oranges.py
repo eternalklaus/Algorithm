cf948608ac91831a4684a69d6d8570672ae53fe8
Jiwon Choi <eternalklaus@naver.com>
2021-04-03 20:01:33 +0900

    Passed

--- Leetcode/994-rotting-oranges.py
+++ Leetcode/994-rotting-oranges.py
@@ -1,34 +1,55 @@
-class Solution(object): 
-    def search_maxtime(self):
-        maxval = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.time[i][j] > maxval:
-                    maxval = self.time[i][j]
-        return maxval
+class Solution(object):
+    def init_values(self, grid):
+        self.grid = grid
+        self.COL = len(grid)
+        self.ROW = len(grid[0])
+        self.queue = []
+        self.days = 0
 
-    def count_rotdate(self, i, j, elapsedtime):
-        if i >= self.height or j >= self.width or i < 0 or j < 0:
-            return
-        print i, j
-        if self.grid[i][j] == 1: # fresh orange!
-            
-            
+    def init_queue(self):
+        for i in xrange(self.COL):
+            for j in xrange(self.ROW):
+                if self.grid[i][j] == 2:
+                    self.queue.append([i, j])
+        self.queue.append('END_OF_ROUND(contaminated succeed)') # marker
+
+    def contamination(self, i, j):
+        is_spreaded = False 
+        if not (i<0 or j<0 or i>=self.COL or j>=self.ROW):
+            if self.grid[i][j] == 1:
+                self.grid[i][j] = 2         # contamination to adjusted oranges...
+                self.queue.append([i,j])    # add newly contaminated orange into queue
+                is_spreaded = True
+        return is_spreaded
+
+    def exist_fresh_orange(self):
+        for i in xrange(self.COL):
+            for j in xrange(self.ROW):
+                if self.grid[i][j] == 1:
+                    return True 
+        return False 
 
     def orangesRotting(self, grid):
-        self.grid = grid 
-        self.height = len(grid)
-        self.width = len(grid[0])
-        self.time = [[0 for i in range(self.width)] for j in range(self.height)]
-        self.elapsedtime = 0
+        self.init_values(grid)
+        self.init_queue()
 
-        for i in xrange(self.height):
-            for j in xrange(self.width):
-                if self.grid[i][j] == 2: # rotten
-                    self.count_rotdate(i, j+1, 1) # east
-                    self.count_rotdate(i+1, j, 1) # south
-                    self.count_rotdate(i, j-1, 1) # west
-                    self.count_rotdate(i-1, j, 1) # north
+        while len(self.queue) != 0:
+            poped           = self.queue.pop(0) # round start
+            is_spreaded     = False
+            while poped != 'END_OF_ROUND(contaminated succeed)': 
+                [i, j] = poped 
+                is_spreaded |= self.contamination(i, j+1)
+                is_spreaded |= self.contamination(i, j-1)
+                is_spreaded |= self.contamination(i+1, j)
+                is_spreaded |= self.contamination(i-1, j)
+                poped = self.queue.pop(0) # round continues...
+
+            if is_spreaded: # append end marker ONLY WHEN new contamination exist
+                self.days += 1
+                self.queue.append('END_OF_ROUND(contaminated succeed)') 
         
-        return self.search_maxtime
-        
+        if self.exist_fresh_orange():
+            return -1
+        else:
+            return self.days
+            
