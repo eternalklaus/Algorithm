cad56c76c5db7af05405169e74a1acbcfed7ed1a
Jiwon Choi <eternalklaus@naver.com>
2021-08-17 08:08:28 +0900

    Passed

--- Leetcode/743-network-delay-time.py
+++ Leetcode/743-network-delay-time.py
@@ -1,17 +1,21 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        for u, v, w in times:
-            graph[u-1].append([v-1, w])
-        dist = [float('inf')] * n 
+    def networkDelayTime(self, times, N, K):
+        from collections import defaultdict 
+        import heapq
 
-        def dijkstra(src): # <= we can use graph inside here
-            for dst, wgt in graph[src]:
-                if dist[src] + wgt < dist[dst]: # update
-                    dist[dst] = dist[src] + wgt
-                    dijkstra(dst) # recursive call only when updated
+        weight = defaultdict(list)
+        for src,dst,wgt in times:
+            weight[src - 1].append([dst - 1,wgt])
         
-        dist[k-1] = 0
-        dijkstra(k-1)
-        ans = max(dist)
-        return ans if ans < float('inf') else -1
+        heap = [[0, K - 1]]
+        dist = [float('inf') for _ in range(N)]
+
+        while heap:
+            srcwgt, src = heappop(heap)
+            if dist[src] > srcwgt:
+                dist[src] = srcwgt 
+                for dst, dstwgt in weight[src]:
+                    heap.append([srcwgt+dstwgt, dst])
+
+        print (dist)
+        return -1 if max(dist) == float('inf') else max(dist)
