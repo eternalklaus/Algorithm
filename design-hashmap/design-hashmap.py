01346ea3e1eba093ad0949e15ee1fbdec9d0b2d2
Jiwon Choi <eternalklaus@naver.com>
2021-09-08 13:42:00 +0900

    Passed. With hash coliision management

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -1,32 +1,58 @@
+class Bucket:
+    def __init__(self):
+        self.bucket = []
+    def store(self, key, val):
+        for idx, [k, v] in enumerate(self.bucket):
+            if k == key:
+                self.bucket[idx] = [key, val]
+                # print ('saving1 .. %d' % key)
+                return
+        # print ('saving2 .. %d' % key)
+        self.bucket.append([key,val])
+        
+    def delete(self, key):
+        for idx, [k, _] in enumerate(self.bucket): 
+            if k == key: 
+                # print ('deleting .. %d' % key)
+                del self.bucket[idx] ### <- remove list component
+                return
+            
+    def read(self, key):
+        # print (self.bucket, key)
+        for [k, v] in self.bucket:
+            if k==key: return v
+        return -1
+            
 class MyHashMap:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.hash = [None] * (10**6+1)
-        
+        self.key_space = 2048
+        self.bucket = [Bucket() for i in range(2048)] ###
+
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        self.hash[key] = [key, value]
+        hash_key = key % self.key_space 
+        self.bucket[hash_key].store(key, value)
         
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        if self.hash[key]:
-            return self.hash[key][1]
-        return -1
-        
+        hash_key = key % self.key_space
+        return self.bucket[hash_key].read(key)
 
     def remove(self, key: int) -> None:
-        self.hash[key] = None
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
+        hash_key = key % self.key_space 
+        self.bucket[hash_key].delete(key)
         
 
 
