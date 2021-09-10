bea949e697b51e8c18d2cda0a6c14c39c35c6dac
Jiwon Choi <eternalklaus@naver.com>
2021-09-10 14:28:00 +0900

    Use heapq

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,9 +2,11 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         from collections import Counter
         cnt = Counter(nums)
-        
-        keys = list(cnt.keys())
-        keys.sort(key = lambda x:cnt[x], reverse = True)
-        
+        '''
+        keys.sort(key = cnt.get, reverse=True)
         return keys[:k]
+        '''
+        nlargest = heapq.nlargest(k, cnt.keys(), key=cnt.get)
+        return nlargest
+        
         
