c19ee6e31b2e1f41828766e801f5fade716cd3c8
Jiwon Choi <eternalklaus@naver.com>
2021-09-21 11:29:03 +0900

    Fix comments

--- minimum-height-trees/minimum-height-trees.py
+++ minimum-height-trees/minimum-height-trees.py
@@ -1,19 +1,19 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        # weird tree only consist of leaves
+        #// weird tree only consist of leaves
         if n <= 2:
             return [x for x in range(n)]
         
-        # make connected graph
+        #// make connected graph
         connected = [[] for _ in range(n)]
         for [i, j] in edges:
             connected[i].append(j)
             connected[j].append(i)
         
-        # find last leaves
-        leaves = [] # only contains the final leaves (has only one node) 
+        #// find last leaves
+        leaves = [] #// only contains the final leaves (has only one node) 
         for v in range(n):
-            if len(connected[v]) == 1: # leaf
+            if len(connected[v]) == 1: #// leaf
                 connected[v]
                 leaves.append(v)
         leaves.append(-1)
@@ -21,23 +21,23 @@ class Solution:
         total = n
         while leaves:
             leaf = leaves.pop(0)
-            if leaf == -1: # end of this level
+            if leaf == -1: #// end of this level
                 if total <= 2:
                     return leaves
                 else:
                     leaves.append(-1)
                     continue
             
-            # get branch that has the leaf
+            #// get branch that has the leaf
             print (connected[leaf])
             branch = connected[leaf][0]
             
-            # drop the leaf
+            #// drop the leaf
             total -= 1
             connected[leaf].remove(branch)
             connected[branch].remove(leaf)
             
-            # if this branch was also a leaf? append leaves
+            #// if this branch was also a leaf? append leaves
             if len(connected[branch]) == 1:
                 leaves.append(branch)
             
