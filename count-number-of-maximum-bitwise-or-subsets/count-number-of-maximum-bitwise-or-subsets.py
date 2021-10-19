32be680dea9c31d371adbdadf31d7581237d86f9
Jiwon <eternalklaus@naver.com>
2021-10-20 06:47:13 +0900

    Time: 65 ms (90.27%), Space: 15.1 MB (29.69%) - LeetHub

---
+++ count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        target = reduce(or_, nums)
+        
+        @cache
+        def fn(i, mask): 
+            """Return number of subsets to get target."""
+            if mask == target: return 2**(len(nums)-i)
+            if i == len(nums): return 0 
+            return fn(i+1, mask | nums[i]) + fn(i+1, mask)
+        
+        return fn(0, 0)
