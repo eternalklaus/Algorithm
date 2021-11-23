8ab37916d4bbee7a33b7c97eda982e9be927f679
Jiwon <eternalklaus@naver.com>
2021-11-23 21:37:13 +0900

    Time: 260 ms (11.46%), Space: 15.5 MB (54.15%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,8 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # O(1) space
-        total = len(nums)
-        counter = Counter(nums)
-        for ch in counter:
-            if counter[ch] > total//2:
-                return ch
+        '''
+        nums.sort()
+        return nums[len(nums)//2]
+        '''
+        
+        # O(1) space - Boyer-Moor Algorithm
+        output = None
+        counter = 0
+        for n in nums:
+            if counter == 0: # change candidate
+                output = n
+                counter = 1
+            else: # upvote/downvote counter
+                if output == n:
+                    counter += 1
+                else:
+                    counter -= 1
+        return output
+                
+            
+            
