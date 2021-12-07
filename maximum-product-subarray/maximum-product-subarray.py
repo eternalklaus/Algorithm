89dd40887f8bc83b024b09d8b5ec879e983cdf4a
Jiwon <eternalklaus@naver.com>
2021-12-07 17:23:57 +0900

    Time: 108 ms (5.13%), Space: 14.4 MB (63.86%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.py
+++ maximum-product-subarray/maximum-product-subarray.py
@@ -1,46 +1,17 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        def getprod(mylist):
-            if len(mylist) == 0: return 0
-            output = 1
-            for n in mylist:
-                output *= n
-            return output
+        def minmax(a, b, c):
+            return min(a,b,c), max(a,b,c)
         
-        def getmaxprod(mynums):
-            if len(mynums) == 0: return 0
-            elif len(mynums) == 1: return mynums[0]
-            
-            # get all of the indexs of minus value
-            minusidxs = []
-            for i, n in enumerate(mynums):
-                if n < 0: minusidxs.append(i)
-            
-            
-            if len(minusidxs) % 2 == 0: # prod would be plus
-                return getprod(mynums)
-            
-            else: # we need to remove one minus value (left or right)
-                mini, maxi = minusidxs[0], minusidxs[-1]
-                return max(getprod(mynums[mini+1:]), getprod(mynums[:maxi]))
+        max_sofar = nums[0]
+        min_sofar = nums[0]
+        output    = nums[0]
         
+        for i in range(1, len(nums)):
+            # use previous calculated value V.S renew the value
+            min_sofar, max_sofar = minmax(max_sofar*nums[i], min_sofar*nums[i], nums[i]) # zero-tolerant
+            output = max(max_sofar, output) # calculated value until certain point should be taken account
         
-        lists = []
-        if 0 not in nums:
-            return getmaxprod(nums)
-        
-        # split by 0
-        temp = [] 
-        for n in nums:
-            if n == 0: # if 0 is appeared
-                if not lists: lists.append([0]) # append 0 either ONLY in the first time
-                lists.append(temp)
-                temp = [] # flush temp
-            else:
-                temp.append(n)
-        lists.append(temp) # append the last list
-        
-        output = nums[0]
-        for l in lists:
-            output = max(output, getmaxprod(l))
         return output
+            
+        
