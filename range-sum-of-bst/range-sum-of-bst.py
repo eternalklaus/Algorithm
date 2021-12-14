c09722c31fe1747ff6e6752c5dde6a44e1397a27
Jiwon <eternalklaus@naver.com>
2021-12-14 14:31:04 +0900

    Time: 216 ms (58.13%), Space: 22.2 MB (85.71%) - LeetHub

--- range-sum-of-bst/range-sum-of-bst.py
+++ range-sum-of-bst/range-sum-of-bst.py
@@ -5,16 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         output = 0
         
-        def dfs(node):
-            nonlocal output
-            if not node: return
+        def searchnode(node, minval, maxval):
+            nonlocal output 
+            # base case
+            if node == None: return
+            
+            # out of range
+            if minval > high or maxval < low: return 
+            
+            lchild = node.left 
+            rchild = node.right 
+            
             if low <= node.val <= high:
-                output += node.val
-            dfs(node.left)
-            dfs(node.right)
+                output += node.val 
+            
+            searchnode(lchild, minval, node.val)
+            searchnode(rchild, node.val, maxval)
         
-        dfs(root)
-        return output
+        
+        searchnode(root, low+1, high-1)
+        return output 
+                
