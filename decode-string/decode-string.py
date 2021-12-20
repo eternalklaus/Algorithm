9dbd94974dd9512bab13019d32e0e132a4c9759e
Jiwon <eternalklaus@naver.com>
2021-12-20 18:47:10 +0900

    Time: 28 ms (83.65%), Space: 14.2 MB (79.83%) - LeetHub

--- decode-string/decode-string.py
+++ decode-string/decode-string.py
@@ -1,18 +1,28 @@
-class Solution(object):
-    def decodeString(self, s):
-        stack = []; curNum = 0; curString = ''
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        s = '*1[' + s + ']'
+        
+        def trimstack():
+            stri, rept = '', ''
+            # pop chars
+            while (c:=stack.pop()) != '[': # caution (! the walrus operator
+                stri = c + stri
+            
+            # pop numbers
+            while '0' <= (n:=stack.pop()) <= '9':
+                rept = n + rept 
+            stack.append(n) # restore more-popped one 
+            rept = int(rept)
+
+            # re-push string 
+            stack.append(rept * stri)
+            
+            
         for c in s:
-            if c == '[':
-                stack.append(curString)
-                stack.append(curNum)
-                curString = ''
-                curNum = 0
-            elif c == ']':
-                num = stack.pop()
-                prevString = stack.pop()
-                curString = prevString + num*curString
-            elif c.isdigit():
-                curNum = curNum*10 + int(c)
+            if c == ']':
+                trimstack()
             else:
-                curString += c
-        return curString
+                stack.append(c)
+        
+        return stack.pop()
