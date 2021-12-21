46b9f520e2e83c9672b8ee84f7ad0365e91f6695
Jiwon <eternalklaus@naver.com>
2021-12-21 11:09:58 +0900

    Time: 1692 ms (93.05%), Space: 39.5 MB (93.99%) - LeetHub

--- maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
+        # Time: O(n) Space: O(1) 
+        N = len(arrays)
+        max1, maxi1 = max( (arrays[idx][-1], idx) for idx in range(N) )
+        min1, mini1 = min( (arrays[idx][0], idx) for idx in range(N) )
+        
+        if maxi1 != mini1:
+            return max1-min1
+        
+        arrays.pop(maxi1) # remove the idx-st value from arrays
+        N = N-1
+        max2, maxi2 = max( (arrays[idx][-1], idx) for idx in range(N) )
+        min2, mini2 = min( (arrays[idx][0], idx) for idx in range(N) )
+        
+        return max(max1-min2, max2-min1)
+        # Time: O(nlogn) Space: O(n)
+        '''
         import heapq
         maxlist = []
         minlist = []
@@ -16,7 +32,7 @@ class Solution:
         (maxval2, maxidx2) = heapq.heappop(maxlist)
         (minval2, minidx2) = heapq.heappop(minlist)
         return max(-maxval-minval2, -maxval2-minval)
-        
+        '''
         
         
         
