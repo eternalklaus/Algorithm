7164e3973f57603cd666d7515bab48a3117e1f85
Jiwon <eternalklaus@naver.com>
2021-12-23 10:14:14 +0900

    Time: 132 ms (22.34%), Space: 15.3 MB (99.32%) - LeetHub

--- course-schedule-ii/course-schedule-ii.py
+++ course-schedule-ii/course-schedule-ii.py
@@ -1,36 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # create prereqs lists
-        classes = [{'BEFO':[], 'NEXT':[]} for _ in range(numCourses)]
-        for [next, befo] in prerequisites:
-            classes[befo]['NEXT'].append(next)
-            classes[next]['BEFO'].append(befo)
+        prergraph = [[] for i in range(numCourses)]
+        nextgraph = [[] for i in range(numCourses)]
+        for [c2, c1] in prerequisites:
+            prergraph[c2].append(c1)
+            nextgraph[c1].append(c2)
         
-        # select first leaves
-        output = []
         stack = []
-        for i in range(numCourses):
-            if classes[i]['BEFO'] == []:
-                output.append(i)
-                stack = classes[i]['NEXT'] + stack 
+        # find the first leaf
+        for i, prereq in enumerate(prergraph):
+            if prereq == []:
+                stack.append(i)
         
-        def canattand(i):
-            for cl in classes[i]['BEFO']:
-                if cl not in output: return False 
-            return True 
-
-        # select next leaves
+        
+        # drop leaves 
+        ###output = set() # set is better since common nexti can be pushed into output at the same time
+        output = []
         while stack:
-            i = stack.pop()
-            if canattand(i) and i not in output:
-                output.append(i)
-                stack = classes[i]['NEXT'] + stack 
-        
-        #print (output)
+            leaf = stack.pop()
+            if prergraph[leaf] == [] and leaf not in output: 
+                output.append(leaf) 
+                for branch in nextgraph[leaf]:
+                    prergraph[branch].remove(leaf) # drop this leaf from all connected branches
+                stack = nextgraph[leaf] + stack # connected branches has posibility of being leaf!
+
         if len(output) == numCourses:
-            return output
+            return output 
         return []
-            
-            
-
-
