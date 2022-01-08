833ef7d301e64c715df153047291ad8728b15524
Jiwon <eternalklaus@naver.com>
2022-01-08 21:58:32 +0900

    Time: 123 ms (9.59%), Space: 15.6 MB (8.22%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -4,11 +4,14 @@ class Solution:
         mins = [nums[0] for _ in range(len(nums))] # to initialize nums[0] 
         maxs = [nums[0] for _ in range(len(nums))] # to initialize nums[0] 
         
+        output = nums[0]
+        
         for i in range(1, len(nums)):
             val1 = mins[i-1] * nums[i]
             val2 = maxs[i-1] * nums[i]
             val3 = nums[i]
             mins[i] = min([val1, val2, val3])
             maxs[i] = max([val1, val2, val3])
+            output = max([output, mins[i], maxs[i]])
         
-        return max(mins + maxs)
+        return output 
