2470a1aa5a163a8040d9ddea1c156fbf3bf0fa22
Jiwon <eternalklaus@naver.com>
2022-01-10 20:22:36 +0900

    Time: 83 ms (15.10%), Space: 14.9 MB (65.03%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,49 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        lo, hi = 0, len(nums) - 1
+        # 4 5 6 6 1 [1] 2 3 4
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if nums[mi] == target: 
+                return True 
+            
+            # left is rotated
+            if nums[lo] > nums[mi]:
+                if target >= nums[lo] or target < nums[mi]:
+                    hi = mi - 1
+                else:
+                    lo = mi + 1
+            
+            # right is rotated
+            elif nums[mi] > nums[hi]: 
+                if target > nums[mi] or target <= nums[hi]:
+                    lo = mi + 1
+                else:
+                    hi = mi - 1
+            
+            # left is straightforward
+            elif nums[lo] < nums[mi]:
+                if nums[lo] <= target < nums[mi]:
+                    hi = mi - 1
+                else: 
+                    lo = mi + 1
+            
+            # right is straighforward
+            elif nums[mi] < nums[hi]:
+                if nums[mi] < target <= nums[hi]:
+                    lo = mi + 1
+                else:
+                    hi = mi - 1
+
+            # cannot be defined!!!
+            # ex) 1 0 [1] 1 1
+            #     -    -    -
+            #     lo   mi   hi
+            else: 
+                hi -= 1
+                lo += 1 
+            
+        if nums[lo] == target: 
+            return True 
+        return False
+                
