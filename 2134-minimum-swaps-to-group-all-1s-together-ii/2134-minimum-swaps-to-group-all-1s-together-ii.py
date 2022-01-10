ea4b530d37152a6eabc5155e65e06806380f6e17
Jiwon <eternalklaus@naver.com>
2022-01-11 05:45:27 +0900

    Time: 1559 ms (25.46%), Space: 18.1 MB (57.41%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        ws = nums.count(1) # window size
+        L = len(nums)
+        
+        
+        num0 = nums[0:0+ws].count(0)
+        output = num0
+        
+        nums = nums+nums
+        for i in range(1, L):
+            if nums[i+ws-1] == 0: # new component is 0! add it!
+                num0 += 1
+            if nums[i-1] == 0: # passed component was 0.. bye..
+                num0 -= 1
+                output = min(num0, output)
+        return output
+            
