8bfb6d65d2a0df065507d684760cd7ddf681afd3
Jiwon <eternalklaus@naver.com>
2022-01-12 06:12:26 +0900

    Time: 146 ms (8.39%), Space: 17.5 MB (32.62%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,25 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         def isoverlapped(itv1, itv2):
-            if itv1[0] <= itv2[0] <= itv1[1]:
-                return True 
-            if itv2[0] <= itv1[0] <= itv2[1]:
-                return True 
-            return False 
+            if itv1[0] > itv2[1]: # ...itv2...itv1... 
+                return False 
+            if itv2[0] > itv1[1]: # ...itv1...itv2... 
+                return False
+            return True
         
         def merge(i):
             nonlocal intervals
             starti = i
-            
             interval = newInterval
             while i < len(intervals) and isoverlapped(interval, intervals[i]):
                 interval = [min(interval[0], intervals[i][0]), max(interval[1], intervals[i][1])]
                 i += 1
-                
             endi = i
             intervals = intervals[:starti] + [interval] + intervals[endi:]
             
-            
+        # O(n)    
         for i in range(len(intervals)):
             if isoverlapped(newInterval, intervals[i]): # overlapped gap exist
                 merge(i)
