9a9e40a48dd4a00ae78712b7fd62b5fad5c58ea2
Jiwon <eternalklaus@naver.com>
2022-01-17 02:25:41 -0800

    Time: 35 ms (73.31%), Space: 14.2 MB (85.64%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,26 +3,14 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        '''
-        0 0 0 0 0 0
-        0 0 0 0 0 1
-        0 0 0 0 0 2
-        0 0 0 0 0 3
-        0 0 0 0 0 4
-        '''
-        N = len(matrix)
-        M = matrix
-        '''
-        for i in range(N // 2):
-            for j in range( i, N - 1 - i):
-        '''
         L = len(matrix)
+        # rotate up-down
         for i in range(L//2):
-            for j in range(i, L - i - 1):
-                # i, j -> j, L-1-i -> L-1-i, L-1-j -> L-1-j, i
-                i1, j1 = i, j 
-                i2, j2 = j, L-i-1
-                i3, j3 = L-i-1, L-j-1
-                i4, j4 = L-j-1, i
+            for j in range(L):
+                matrix[i][j], matrix[L-1-i][j] = matrix[L-1-i][j] , matrix[i][j]
                 
-                matrix[i1][j1], matrix[i2][j2], matrix[i3][j3], matrix[i4][j4] = matrix[i4][j4], matrix[i1][j1], matrix[i2][j2], matrix[i3][j3]
+        # rotate down-right (change i, j)
+        for i in range(L):
+            for j in range(i, L):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] 
+        
