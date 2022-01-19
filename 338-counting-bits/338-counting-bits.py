93105ae0a735f984cf411e37d697030d80340c96
Jiwon <eternalklaus@naver.com>
2022-01-20 06:37:15 +0900

    Time: 116 ms (46.87%), Space: 20.9 MB (36.49%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,12 +1,17 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # repetitive length = 1, 2, 4, 8, ..
         L = n + 1
         if n == 0:
             return [0]
         if n == 1:
             return [0, 1]
         
+        # Add values one by one. leverage that output has below pattern
+        # [0, 1] [0+1, 1+1]
+        # [0, 1,    1,   2] [1+1, 2+1, 2+1, 3+1]
+        # ...
+        # such pattern repeates every 1, 2, 4, 8, 16 ..
+        '''
         output, gap, counter = [0, 1], 2, 2
         for i in range(2, L):
             if counter == 0:
@@ -15,5 +20,9 @@ class Solution:
             output.append(output[i-gap] + 1)
             counter -= 1
         return output 
-            
+        '''
+        output = [0, 1]
+        while len(output) < L:
+            output += [x+1 for x in output]
+        return output[:L]
         
