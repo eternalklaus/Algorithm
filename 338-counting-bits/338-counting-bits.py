c4887a5207c3fe569c481cf68c8b1c849f84eb40
Jiwon <eternalklaus@naver.com>
2022-01-20 06:26:22 +0900

    Time: 134 ms (36.75%), Space: 20.9 MB (36.49%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,23 +1,19 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # 0 1 10 11 100 101 110 111 1000 1001 1010 1011 
-        # 0 1. 1  2.  1.  2.  2.  3.   1.   2.   2.   3
-        # repeating...
-        # 1, 2, 4, 8, 16, ...
-        
-        
+        # repetitive length = 1, 2, 4, 8, ..
+        L = n + 1
         if n == 0:
             return [0]
         if n == 1:
             return [0, 1]
         
-        output = [0, 1]
-        counter, roundnum = 0, 1
-        for i in range(2, n+1):
+        output, gap, counter = [0, 1], 2, 2
+        for i in range(2, L):
             if counter == 0:
-                counter = 2 ** roundnum # reset counter 
-                roundnum += 1 
-            output.append(1 + output[i- (2**roundnum)])
+                counter = gap
+                gap = gap * 2 
+            output.append(output[i-gap] + 1)
             counter -= 1
         return output 
             
+        
