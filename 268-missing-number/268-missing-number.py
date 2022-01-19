5fd32a9d9b3a0566a7bed57f8ffc7a86f4b4f458
Jiwon <eternalklaus@naver.com>
2022-01-20 05:10:24 +0900

    Time: 211 ms (33.92%), Space: 15.4 MB (80.11%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,8 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         L = len(nums)
+        
+        # Time: O(nlogn) Space: O(1)
+        '''
         nums.sort()
         for i in range(L):
             if nums[i] != i:
                 return i
         return L
+        '''
+        # [Follow up]
+        # Time: O(n) space: O(1)
+        total = L * (L+1) // 2 
+        return total - sum(nums)
