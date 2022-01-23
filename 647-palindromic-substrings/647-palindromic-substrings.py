d68e17193e8c0f17b9b531805435467a126aacc9
Jiwon <eternalklaus@naver.com>
2022-01-24 05:24:28 +0900

    Time: 116 ms (92.95%), Space: 14.3 MB (66.36%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,7 +1,8 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        # Time complexity: O(N^2) / Space complexity: O(n^2) including stacks of each function call 
+        '''
         output, L = 0, len(s)
-        # @cache # <- prevents duplicated calculation 
         def expand_palindrom(first, last):
             nonlocal output 
             
@@ -12,10 +13,24 @@ class Solution:
             if s[first] == s[last]:
                 output += 1                
                 expand_palindrom(first-1, last+1)
-                return True 
+                return 
         
         for i in range(L):
             expand_palindrom(i, i) # odd length of palindrom
             expand_palindrom(i-1, i) # even length of palindrom 
             
+        return output 
+        '''
+        output, L = 0, len(s)
+        for i in range(L):
+            # odd number of palindrom
+            li, ri = i, i
+            while li >= 0 and ri < L and s[li] == s[ri]:
+                li, ri = li-1, ri+1
+                output += 1
+            # even number of palindrom
+            li, ri = i-1, i
+            while li >= 0 and ri < L and s[li] == s[ri]:
+                li, ri = li-1, ri+1
+                output += 1
         return output 
