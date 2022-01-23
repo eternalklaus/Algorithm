8bf3b0039e5982f74e99c7989afd8b3340baa7b1
Jiwon <eternalklaus@naver.com>
2022-01-23 17:19:24 +0900

    Time: 96 ms (86.22%), Space: 17.2 MB (87.12%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,19 +2,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # key is sorted one of strs. 
         from collections import defaultdict
-        def sortstr(s):
-            result = [c for c in s]
-            result.sort()
-            return ''.join(result)
-            
         outputdict = defaultdict(list)
         for s in strs:
-            ssort = sortstr(s)
-            outputdict[ssort].append(s)
-        '''
-        print (list(outputdict.keys()))
-        print (list(outputdict.values()))
-        '''
+            ss = sorted(s)
+            outputdict[''.join(ss)].append(s)
         return list(outputdict.values())
             
             
