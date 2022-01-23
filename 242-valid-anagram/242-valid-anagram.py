b5c3fdfd2edff6ff7821df0556fbcf7464356422
Jiwon <eternalklaus@naver.com>
2022-01-23 16:55:35 +0900

    Time: 108 ms (7.62%), Space: 14.6 MB (57.14%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,8 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        # Assuming that length of each string is m, n,
+        # Time complexity: O(mlogm+nlogn)
+        # Space complecity: O(m+n)
+        '''
         s = [c for c in s]
         t = [c for c in t]
         s.sort() 
         t.sort() 
         if s==t: return True 
         return False 
+        '''
+        
+        # Hash table
+        from collections import Counter 
+        counter = Counter()
+        for c in s:
+            counter[c] += 1
+        
+        if len(s)!=len(t): return False # two string should be same length
+        for c in t:
+            counter[c] -= 1
+            if counter[c] < 0:
+                return False 
+        return True 
