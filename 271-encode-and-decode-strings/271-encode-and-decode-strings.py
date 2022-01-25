ab4ce3e81609a755ed505909f8dd708dfcf2b9eb
Jiwon <eternalklaus@naver.com>
2022-01-25 20:53:25 +0900

    Time: 68 ms (69.56%), Space: 14.9 MB (10.80%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -2,13 +2,27 @@ class Codec:
     def encode(self, strs: [str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        return strs
-        
-
+        output = ''
+        for s in strs:
+            output = output + str(len(s)).zfill(3) + s
+        return output 
+    
     def decode(self, s: str) -> [str]:
         """Decodes a single string to a list of strings.
         """
-        return s
+        i, L, output = 0, len(s), []
+        while True:
+            if len(s) == 0: break
+            # read 3 bytes
+            length = int(s[:3])
+            # strip 3 bytes
+            s = s[3:]
+            # append output with the length
+            output.append(s[:length])
+            # strip length bytes
+            s = s[length:]
+        return output  
+        
         
 
 
