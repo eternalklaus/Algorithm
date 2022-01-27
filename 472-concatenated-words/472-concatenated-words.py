527efe665fb1e3d445019e6883a218f61f0c07f6
Jiwon <eternalklaus@naver.com>
2022-01-28 05:32:55 +0900

    Time: 6124 ms (5.05%), Space: 33.1 MB (46.42%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -1,22 +1,19 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        wordset = set(words)
-        
-        @cache
+        wordset = set(words) ###1
+        @cache ###2
         def dfs(word):
-            for i in range(1,len(word)):
-                prefix, suffix = word[:i], word[i:]
-                # print (prefix, suffix)
-                if prefix in wordset and suffix in wordset:
+            for i in range(1, len(word)): ### 최초에 전달되는 word부터 쪼개야 함. 바로 word in wordset 때리면 안됨~
+                if word[:i] in wordset and word[i:] in wordset:
                     return True 
-                if prefix in wordset and dfs(suffix):
+                if word[:i] in wordset and dfs(word[i:]):
                     return True 
+                ### if dfs(word[:i]) and word[i:] in wordset: ###3
             return False 
-        
+    
         output = []
-        for word in wordset:
-            if dfs(word): 
+        for word in words:
+            if dfs(word):
                 output.append(word)
         return output 
-        
-                
+                    
