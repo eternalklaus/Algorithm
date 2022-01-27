db4be6423a2cc88ef1b5bef4d8aad6520acb6bc0
Jiwon <eternalklaus@naver.com>
2022-01-27 14:25:40 +0900

    Time: 184 ms (33.47%), Space: 14.8 MB (89.19%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -5,40 +5,41 @@ class Solution: #TODO: 이거 토대로 풀이 올리기
         output = s+'a'
         outputlen = len(output)
         counter = Counter(t)
+        toclear = len(counter) ### unique chars to be cleared!
         
         def increase(c):
             nonlocal counter
-            if c in counter: 
+            if c in counter: ### O(1), if it's comparing with list, O(n) 
                 counter[c] += 1
         
         def decrease(c):
             nonlocal counter
             if c in counter: 
                 counter[c] -= 1
-        
-        def allzeroed():
-            for v in counter.values():
-                if v > 0: return False 
-            return True 
-            
-        decrease(s[0]) # initialize
-        
+
+
+        if counter[s[0]] == 1: toclear -= 1
+        decrease(s[0]) # initialize        
+
         while True:
-            # decrease window with moving first --> last
-            if allzeroed():
+            # decrease window with moving [first --> last]
+            if toclear == 0:
                 # update output 
-                if last - first + 1 < outputlen: # if last - first + 1< len(output):
+                if last - first + 1 < outputlen: ### O(1), if last - first + 1< len(output):
                     output = s[first:last+1]
                     outputlen = len(output)
                 # move first
                 increase(s[first])
+                if counter[s[first]] == 1: toclear += 1 # zero -> one increased
                 first += 1 
                 
-            # increase window with moving first last -->
-            else:
+            # increase window with moving [first last] -->
+            else: 
                 last += 1
                 if last >= L: break 
+                if counter[s[last]] == 1: toclear -= 1 
                 decrease(s[last])
+                # if counter[s[last]] == 0: toclear -= 1 # 여기다하면 원래 0인놈들과 구분이 안되니 안돼!
         
         if outputlen > len(s):
             return ''
