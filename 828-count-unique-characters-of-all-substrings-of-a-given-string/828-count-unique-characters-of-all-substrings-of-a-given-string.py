5bf820945fba5514c47feba2d3eb17eed9de54b9
Jiwon <eternalklaus@naver.com>
2022-01-27 16:14:27 +0900

    Time: 893 ms (9.73%), Space: 18.8 MB (23.89%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -5,20 +5,22 @@ class Solution:
         for i, c in enumerate(s):
             cdict[c].append(i)
         
-        # ABCD -> A ABC A
-        # ABCD -> B ABC B 
+        # A: ABCD -> A ABC A
+        #            -1 0  3
+        #          value =
+        # B: ABCD -> B ABC B 
         # ...
+        # 
         output = 0
         for c in cdict:
             idxlist = cdict[c]
             idxlist.insert(0, -1)
-            idxlist.append(L)
-            #print (idxlist)
+            idxlist.append(L) 
 
             for idx2 in range(1, len(idxlist)-1):
                 idx1 = idx2-1
                 idx3 = idx2+1
-                val = (idxlist[idx2]-idxlist[idx1]-1+1) * (idxlist[idx3]-idxlist[idx2]-1+1) # including removing case. if length is 2, multiply 3
-                if val > 0:
-                    output += val
+                # including removing case. if length is 2, multiply 3
+                output += (idxlist[idx2]-idxlist[idx1]-1+1) * (idxlist[idx3]-idxlist[idx2]-1+1) 
+                
         return output
