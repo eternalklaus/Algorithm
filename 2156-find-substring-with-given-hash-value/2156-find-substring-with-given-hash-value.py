b91773a9112ee1645d464b0370b7e2f4ff9f4c73
Jiwon <eternalklaus@naver.com>
2022-01-31 07:37:52 +0900

    Time: 1082 ms (9.09%), Space: 14.3 MB (100.00%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
@@ -1,17 +1,17 @@
 class Solution:
-    def subStrHash(self, s, p, m, k, hashValue):
+    def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
         def val(c):
             return ord(c) - ord('a') + 1
+        
+        
+        L = len(s)
+        hashval = 0
+        for i in range(L-1, -1, -1):
+            hashval = (hashval * power + val(s[i])) % modulo
+            if i < L-k:
+                hashval -= val(s[i+k]) * pow(power, k, modulo)
+                hashval %= modulo
             
-        res = n = len(s)
-        pk = pow(p,k,m)
-        cur = 0
-
-        for i in range(n - 1, -1, -1):
-            cur = (cur*p + val(s[i])) % m
-            if i + k < n:
-                cur = (cur - val(s[i+k])*pk ) % m # minus modulo also possible...!!!!!!!! 
-                #print (cur)
-            if cur == hashValue:
-                res = i
-        return s[res: res + k]
+            if hashval == hashValue:
+                output = s[i:i+k]
+        return output 
