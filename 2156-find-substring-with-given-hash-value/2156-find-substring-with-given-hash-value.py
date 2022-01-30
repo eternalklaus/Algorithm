f70cb40092bdc79300bde0d9b699d04d6e502dd5
Jiwon <eternalklaus@naver.com>
2022-01-31 08:15:08 +0900

    Time: 631 ms (9.09%), Space: 14.2 MB (100.00%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
@@ -3,13 +3,14 @@ class Solution:
         def val(c):
             return ord(c) - ord('a') + 1
         
-        
         L = len(s)
         hashval = 0
+        pk = pow(power, k, modulo)
+        
         for i in range(L-1, -1, -1):
             hashval = (hashval * power + val(s[i])) % modulo
             if i < L-k:
-                hashval -= val(s[i+k]) * pow(power, k, modulo)
+                hashval -= val(s[i+k]) * pk
                 hashval %= modulo
             
             if hashval == hashValue:
