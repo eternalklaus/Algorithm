1351ba939080c8318b439be73b891569bd1e75e9
Jiwon <eternalklaus@naver.com>
2022-02-02 06:53:49 +0900

    Time: 101 ms (39.40%), Space: 24 MB (8.15%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,28 +8,33 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # 1. Make it linked list -> list
-        nodes, node = [], head 
+        # 1. Get middle of list 
+        fast, slow = head, head 
+        while fast and fast.next:
+            prevslow = slow 
+            slow = slow.next ### move this first
+            fast = fast.next.next 
+        # prevslow.next = None 
+        middle = slow 
+        
+        # slow = 1, 2, 3, 4, 5, 6
+        # fast = 4, 5, 6
+        
+        # 2. Reverse second list
+        prevnode = None 
+        node = middle 
         while node:
-            nodes.append(node)
-            node = node.next 
+            nextnode = node.next 
+            node.next = prevnode 
+            prevnode = node 
+            node = nextnode 
+        slow = prevnode # start of node is prevnode 
+        # slow = 6, 5, 4
         
-        # 2. Order list
-        ordered, L = [], len(nodes)
-        li, ri = 0, L-1
-        while li < ri:
-            ordered.append(nodes[li])
-            ordered.append(nodes[ri])
-            li += 1
-            ri -= 1
-        # 2.2 append last node 
-        if li == ri:
-            ordered.append(nodes[li])
         
-        # 3. Reconnect nodes
-        i = -1 
-        for i in range(L-1):
-            ordered[i].next = ordered[i+1]
-        ordered[i+1].next = None 
-            
-            
+        # 3. Interleave two lists 
+        node1 = head 
+        node2 = slow 
+        while node2.next:
+            node1.next, node1 = node2, node1.next 
+            node2.next, node2 = node1, node2.next 
