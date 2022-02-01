25a677002b66a1654e56186776ae3c8594075164
Jiwon <eternalklaus@naver.com>
2022-02-02 07:19:35 +0900

    Time: 61 ms (13.89%), Space: 13.9 MB (93.22%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,24 +5,20 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # keep queue in size n+1, if more then n, remove the front component. 
-        # in the end, cut node[1] using node[0]
-        def printqueue(queue):
-            for q in queue:
-                print ('%d '% q.val, end='')
-            print ('')
+        fakehead = ListNode(0, head)
+        slow, fast = fakehead, fakehead
         
-        fakehead = ListNode(-1, head)
-        node = fakehead  
-        queue = []
+        # n times of cycle is needed to fill the gap
+        cycle = 0
+        while fast.next: ###<= 
+            if cycle >= n: 
+                slow = slow.next 
+                fast = fast.next 
+            else:
+                fast = fast.next 
+            cycle += 1
         
-        while node: 
-            queue.append(node)
-            if len(queue) > n + 1: # queue contains [node - 1] [node] 
-                queue.pop(0)
-            # printqueue(queue)
-            node = node.next
-        
-        queue[0].next = queue[1].next 
-        return fakehead.next 
+        # [[1]]
+        slow.next = slow.next.next 
+        return fakehead.next
             
