4d472ef8d03352c7c27a2b6a00a6224f0cf0e52f
Jiwon <eternalklaus@naver.com>
2022-02-02 20:02:10 +0900

    Time: 1737 ms (5.01%), Space: 15.3 MB (81.99%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getFood(self, grid: List[List[str]]) -> int:
+        C, R = len(grid), len(grid[0])
+        (i, j) = [[i, j] for i in range(C) for j in range(R) if grid[i][j] == '*'][0] ###<-
+        outputs = []
+        
+        queue = [(0, i, j)]
+        while queue:
+            steps, i, j = heappop(queue)
+            if not 0<=i<C or not 0<=j<R or grid[i][j] == 'X': 
+                continue 
+                
+            if grid[i][j] == '#':
+                outputs.append(steps)
+                
+            grid[i][j] = 'X' # mark visited location(this is cheapest way to reach here)
+            heappush(queue, (steps+1, i+1, j))
+            heappush(queue, (steps+1, i-1, j))
+            heappush(queue, (steps+1, i, j+1))
+            heappush(queue, (steps+1, i, j-1))
+        
+        print (outputs)
+        if not outputs: 
+            return -1
+        return min(outputs)
+            
