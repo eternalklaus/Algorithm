61c6c3e699a0e23765302289a8c40d3efff5b2bd
Jiwon <eternalklaus@naver.com>
2022-02-04 21:05:26 +0900

    Time: 99 ms (11.24%), Space: 18.1 MB (28.97%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -6,25 +6,22 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # devide and conquar
-        # try preorder search from the left of node, 
-        # and if we fill the count, return the node's value immidiatly
         
-        output, counter = -1, 0
-        
-        def inorder(node):
-            nonlocal output, counter 
-            if not node: return 
+        node = root 
+        stack = [root]
+        while stack: 
+            # push left nodes indescreetly
+            while node:
+                stack.append(node)
+                node = node.left 
             
-            inorder(node.left) 
-            ###
-            # print (node.val, counter)
-            counter += 1
-            if counter == k: 
-                output = node.val 
-                return 
-            ###
-            inorder(node.right)
+            # pop node gently
+            node = stack.pop()
+            k -= 1 
+            if k == 0: return node.val 
+            
+            # now move to right node gently
+            node = node.right 
         
-        inorder(root)
-        return output 
+        return -1
+            
