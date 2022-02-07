a4a684ac8c71d6ca0e8d2e08b44136098b6fe056
Jiwon <eternalklaus@naver.com>
2022-02-07 15:01:03 +0900

    Time: 652 ms (38.19%), Space: 38.7 MB (38.84%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,19 +1,26 @@
 class SnapshotArray:
-    from collections import defaultdict 
-    
+
     def __init__(self, length: int):
-        self.data = defaultdict(int) # [0] * length  
-        self.snapshots = []
+        import bisect
+        self.next_snap_id = 0
+        self.array = [[[-1, 0]] for _ in range(length)] 
 
     def set(self, index: int, val: int) -> None:
-        self.data[index] = val 
+        history = self.array[index]
+        if history[-1][0] == self.next_snap_id: # snapshot hasn't shot since previous update 
+            history[-1][1] = val # update data
+        else:
+            history.append([self.next_snap_id, val]) # append current snapshot info  and data
 
-    def snap(self) -> int: # returns stap_id 
-        self.snapshots.append(self.data.copy())###
-        return len(self.snapshots)-1
-
+    def snap(self) -> int:
+        self.next_snap_id += 1
+        return self.next_snap_id - 1
+        
     def get(self, index: int, snap_id: int) -> int:
-        return self.snapshots[snap_id][index]
+        history = self.array[index]
+        # print (history, snap_id)
+        idx = bisect.bisect(history, [snap_id, 10**9+1]) # if exist, return idx+1. if not exist, return existclose + 1
+        return history[idx-1][1]
 
 
 # Your SnapshotArray object will be instantiated and called as such:
