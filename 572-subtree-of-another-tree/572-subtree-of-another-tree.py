211fdf79f54f0c9aa2de51d9883cdfeb74b8c0e6
Jiwon <eternalklaus@naver.com>
2022-02-07 16:35:53 +0900

    Time: 220 ms (23.21%), Space: 14.7 MB (96.11%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        '''
         if not subRoot: return True 
         
         @cache 
@@ -16,11 +17,28 @@ class Solution:
                 if root.val == subroot.val:
                     if (compare(root.left, subroot.left) and compare(root.right, subroot.right)): # continue previous one 
                         return True 
-                return compare(root.left, subRoot) or compare(root.right, subRoot) # previous compare result or new start 
-                
+                return compare(root.left, subRoot) or compare(root.right, subRoot) # previous compare result or new start      
             return False # None and TreeNode or vice versa
             
-            
         return compare(root, subRoot)
+        '''
         
+        def checknode(root, subroot):
+            if not root and not subroot: return True 
+            if root and subroot:
+                if root.val == subroot.val:
+                    return checknode(root.left, subroot.left) and checknode(root.right, subroot.right)
+            return False 
+        
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            
+            if not node: continue 
+            if checknode(node, subRoot):
+                return True
+            queue.append(node.left)
+            queue.append(node.right)
+        return False 
+            
             
