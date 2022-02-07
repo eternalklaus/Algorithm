ab202c5a3d6e9d32cd5d0c8676850bd07b8883ef
Jiwon <eternalklaus@naver.com>
2022-02-07 20:03:05 +0900

    Time: 106 ms (92.97%), Space: 15.1 MB (60.32%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,27 +6,7 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        
-        
-        
-        '''
-        def serialize(node):
-            if not node:
-                serial = '#'
-            else:
-                serial  = '!'
-                serial += str(node.val)
-                serial += '!'
-                serial += serialize(node.left)
-                serial += serialize(node.right)
-            
-            return serial 
-        
-        if serialize(subRoot) in serialize(root): return True 
-        return False 
-        '''
-        def sy(nd):return f"A{nd.val}#{sy(nd.left)}{sy(nd.right)}"if nd else"Z"
-        return sy(subRoot)in sy(root)
-                
-            
-            
+        def serial(node):
+            if not node: return '#'
+            return f'|{node.val}|{serial(node.left)}|{serial(node.right)}'
+        return serial(subRoot) in serial(root)
