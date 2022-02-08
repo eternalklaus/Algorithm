4b2f250a4e7b3b2ac1e2f37da88070dec946956d
Jiwon <eternalklaus@naver.com>
2022-02-08 22:07:02 +0900

    Time: 9589 ms (6.82%), Space: 16.4 MB (19.36%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,35 +1,64 @@
-class TrieNode:
-    def __init__(self):
-        self.children = defaultdict(TrieNode)
-        self.word = None
-
-    def addWord(self, word):
-        cur = self
-        for c in word:
-            cur = cur.children[c]
-        cur.word = word
-
+class Trie:
+    import string 
+    def __init__(self): ###<=
+        self.char = {}
+        self.end = False 
+        
+    
 class Solution:
+    
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        m, n = len(board), len(board[0])
-        DIR = [0, 1, 0, -1, 0]
-        trieNode = TrieNode()
-        ans = []
+        output = []
+        root = Trie()
         for word in words:
-            trieNode.addWord(word)
+            node = root 
+            for c in word:
+                if not node.char.get(c):
+                    node.char[c] = Trie() 
+                node = node.char[c]
+            node.end = word ###<-!!! 
+        
+        def prunechild(node): # cut dead branch(has no 'end')
+            if node.end: return False 
+            # if there's no accumulated 'end', this branch is dealt as dead branch 
+            if not node.char: return True 
 
-        def dfs(r, c, cur):
-            if r < 0 or r == m or c < 0 or c == n or board[r][c] not in cur.children: return
-            orgChar = board[r][c]
-            cur = cur.children[orgChar]
-            board[r][c] = '#'  # Mark as visited
-            if cur.word != None:
-                ans.append(cur.word)
-                cur.word = None  # Avoid duplication!
-            for i in range(4): dfs(r + DIR[i], c + DIR[i + 1], cur)
-            board[r][c] = orgChar  # Restore to org state
+            no_end_in_this_tree = True 
+            for c in node.char:
+                if prunechild(node.char[c]):
+                    del node.char[c]
+                else: 
+                    no_end_in_this_tree = False 
+            return no_end_in_this_tree
+                
+        def search(i, j, node):
+            nonlocal output 
+            # base case 
+            if not 0<=i<len(board): return False
+            if not 0<=j<len(board[0]): return  False
 
-        for r in range(m):
-            for c in range(n):
-                dfs(r, c, trieNode)
-        return ans
+            c = board[i][j]
+            childnode = node.char.get(c)
+            if not childnode: return  False
+
+            if childnode.end: # exist end!
+                output.append(childnode.end)
+                childnode.end = False 
+            
+            res = False 
+            board[i][j] = '$' 
+            res |= search(i+1, j, childnode)
+            res |= search(i-1, j, childnode)
+            res |= search(i, j+1, childnode)
+            res |= search(i, j-1, childnode) 
+            board[i][j] = c
+            
+            if res:
+                prunechild(node)
+            return res 
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                search(i, j, root)
+        return output
+
