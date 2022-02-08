e0b75fb42ab87f0cc115dcbd62bd2df25b547ace
Jiwon <eternalklaus@naver.com>
2022-02-08 22:10:00 +0900

    Time: 3919 ms (57.95%), Space: 14.1 MB (99.80%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -18,45 +18,29 @@ class Solution:
                 node = node.char[c]
             node.end = word ###<-!!! 
         
-        def prunechild(node): # cut dead branch(has no 'end')
-            if node.end: return False 
-            # if there's no accumulated 'end', this branch is dealt as dead branch 
-            if not node.char: return True 
-
-            no_end_in_this_tree = True 
-            for c in node.char:
-                if prunechild(node.char[c]):
-                    del node.char[c]
-                else: 
-                    no_end_in_this_tree = False 
-            return no_end_in_this_tree
-                
         def search(i, j, node):
             nonlocal output 
             # base case 
-            if not 0<=i<len(board): return False
-            if not 0<=j<len(board[0]): return  False
+            if not 0<=i<len(board): return  
+            if not 0<=j<len(board[0]): return  
 
             c = board[i][j]
             childnode = node.char.get(c)
-            if not childnode: return  False
+            if not childnode: return  
 
             if childnode.end: # exist end!
                 output.append(childnode.end)
                 childnode.end = False 
-            
-            res = False 
+                if not childnode.char: ###
+                    del node.char[c]
+
             board[i][j] = '$' 
-            res |= search(i+1, j, childnode)
-            res |= search(i-1, j, childnode)
-            res |= search(i, j+1, childnode)
-            res |= search(i, j-1, childnode) 
+            search(i+1, j, childnode)
+            search(i-1, j, childnode)
+            search(i, j+1, childnode)
+            search(i, j-1, childnode) 
             board[i][j] = c
             
-            if res:
-                prunechild(node)
-            return res 
-
         for i in range(len(board)):
             for j in range(len(board[0])):
                 search(i, j, root)
