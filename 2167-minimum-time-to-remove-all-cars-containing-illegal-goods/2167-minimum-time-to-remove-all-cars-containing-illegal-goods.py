da2aba8c7ff272f818fd1ed82088f66c0b53ef1a
Jiwon <eternalklaus@naver.com>
2022-02-11 19:45:18 +0900

    Time: 2016 ms (84.83%), Space: 16.4 MB (77.76%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.py
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.py
@@ -1,48 +1,16 @@
 class Solution:
     def minimumTime(self, s: str) -> int:
+        leftmin, lrmin = 0, s.count('1')*2
         L = len(s)
-        pre, suf = [0] * L, [0] * L
-        
-        val = 0
         for i in range(L):
-            if s[i]=='1': val += 1
-            else: val -= 1
-            pre[i] = val 
+            walk = i+1
+            walkandfly = leftmin + (s[i]=='1')*2 
+            
+            leftmin = min(walk, walkandfly)
+            
+            right = L - 1 - i
+            
+            lrmin = min(lrmin, leftmin+right)
         
-        val = 0
-        for i in range(L-1, -1, -1):
-            if s[i]=='1': val += 1
-            else: val -= 1
-            suf[i] = val 
-        '''
-        print (pre)
-        print (suf)
-        '''
-        # maximum value of pre[i] + suf[i+1]
-        lmax, rmax = [-L] * L, [-L] * L
-        
-        maxval = 0 # pre[0] # not taking any value
-        for i in range(L):
-            maxval = max(maxval, pre[i])
-            lmax[i] = maxval
-        
-        maxval = 0 # suf[L-1] # not taking any value
-        for i in range(L-1, -1, -1):
-            maxval = max(maxval, suf[i])
-            rmax[i] = maxval
-        
-        # print (lmax)
-        # print (rmax)
-        
-        # get maximum value of lmax[i] + rmax[i+1]
-        maxsum = 0
-        maxsum = max([0, rmax[0], lmax[L-1]])
-        for i in range(L-1):
-            maxsum = max(maxsum, lmax[i]+rmax[i+1])
-        
-        # print (maxsum)
-        # subtract maxsum
-        
-        total = 2 * s.count('1')
-        return total - maxsum
+        return lrmin
             
