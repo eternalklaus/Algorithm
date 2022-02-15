d1a2782f7fbdac261f9782c20686f48aa7eccade
Jiwon <eternalklaus@naver.com>
2022-02-15 22:07:33 +0900

    Time: 4329 ms (10.52%), Space: 48.2 MB (75.19%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
@@ -1,20 +1,31 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        ROW, COL = len(points), len(points[0])
-        points.insert(0, [0 for j in range(COL)])
-        rlgrid  = [[0 for j in range(COL)] for i in range(ROW+1)]
-        lrgrid  = [[0 for j in range(COL)] for i in range(ROW+1)]
+        I, J = len(points), len(points[0])
         
-        for i in range(1, ROW+1):
-            for j in range(COL):
-                points[i][j] = points[i][j] + max(lrgrid[i-1][j], rlgrid[i-1][j])
+        temp = [0] * J
+        
+        for i in range(I):
+            # accumulate upper line
             
-            lrgrid[i][0] = points[i][0]
-            for j in range(1, COL):
-                lrgrid[i][j] = max(lrgrid[i][j-1] - 1, points[i][j])
-
-            rlgrid[i][COL-1] = points[i][COL-1]
-            for j in range(COL-2, -1, -1):
-                rlgrid[i][j] = max(rlgrid[i][j+1] - 1, points[i][j])
             
-        return max(points[ROW])
+            for j in range(J):
+                points[i][j] = points[i][j] + temp[j]
+            
+            # clear temp 
+            temp = [0] * J
+            
+            # get lmax 
+            temp[0] = points[i][0]
+            for j in range(1, J):
+                temp[j] = max(temp[j-1] - 1, points[i][j])
+                
+            # get rmax 
+            temp[-1] = max(points[i][-1], temp[-1])
+            for j in range(J-2, -1, -1):
+                temp[j] = max(temp[j+1] - 1, temp[j]) # compare precalculated(lr) one
+            
+        return max(points[-1])
+    
+    # [5,2,1,2]
+    # [2,1,5,2]
+    # [5,5,5,0]
