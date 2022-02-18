1d372bfb7238c792212d021007cf633072dc6888
Jiwon <eternalklaus@naver.com>
2022-02-19 06:12:15 +0900

    Time: 641 ms (14.75%), Space: 15.6 MB (41.86%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,44 +1,40 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        COL, ROW = len(heights), len(heights[0])
-
-        def ingrid(i, j):
-            if 0<=i<COL and 0<=j<ROW: return True 
-            return False 
-
+        I, J = len(heights), len(heights[0])
         Pacific, Atlantic = set(), set()
-        pq, aq = [], [] # queue for visit
-
-        # 0. Initialize each beaches
-        for j in range(ROW):
-            pq.append((0, j))
-            aq.append((COL-1, j))
-        for i in range(COL):
-            pq.append((i, 0))
-            aq.append((i, ROW-1))
         
-        # 1. Pacific
-        while pq:
-            (pi, pj) = pq.pop()
-            Pacific.add((pi, pj))
-
-            for i, j in [(pi+1, pj), (pi-1, pj), (pi, pj+1), (pi, pj-1)]:
-                if not ingrid(i, j): continue 
-                if (i, j) in Pacific: continue 
-                if heights[pi][pj] <= heights[i][j]: # lets climb up! 
-                    pq.append((i, j))
+        def search(i, j, sea):
+            
+            for newi, newj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                if not 0<=newi<I or not 0<=newj<J: continue 
+                if (newi, newj) in sea: continue 
+                
+                if heights[newi][newj] >= heights[i][j]: 
+                    sea.add((newi, newj))
+                    search(newi, newj, sea)
+                            
+        # top
+        i = 0
+        for j in range(J):
+            Pacific.add((i, j))
+            search(i, j, Pacific)
         
-        # 2. Atlantic
-        while aq:
-            (ai, aj) = aq.pop()
-            Atlantic.add((ai, aj))
-
-            for i, j in [(ai+1, aj), (ai-1, aj), (ai, aj+1), (ai, aj-1)]:
-                if not ingrid(i, j): continue 
-                if (i, j) in Atlantic: continue 
-                if heights[ai][aj] <= heights[i][j]: # lets climb up! 
-                    aq.append((i, j))
+        # left 
+        j = 0
+        for i in range(I):
+            Pacific.add((i, j))
+            search(i, j, Pacific)
         
-        # Common set of Pacific and Atlantic
-        # return [x for x in Pacific if x in Atlantic]
-        return Pacific & Atlantic
+        # bottom
+        i = I-1
+        for j in range(J):
+            Atlantic.add((i, j))
+            search(i, j, Atlantic)
+            
+        # right 
+        j = J-1
+        for i in range(I):
+            Atlantic.add((i, j))
+            search(i, j, Atlantic)
+        
+        return [coord for coord in Atlantic if coord in Pacific]
