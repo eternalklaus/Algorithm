9f0a803b78b0de31d64c975e79870049f3d48058
Jiwon <eternalklaus@naver.com>
2022-02-18 11:18:17 +0900

    Time: 63 ms (31.96%), Space: 14 MB (87.29%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -3,10 +3,11 @@ class Solution:
         
         # if replacement is overlapped, stop operation
         L, output, prevend = len(indices), '', 0
-        index = sorted([i for i in range(L)], key = lambda x: indices[x])
+        #index = sorted([i for i in range(L)], key = lambda x: indices[x])
         
-        for i in index:
-            idx, src, tgt = indices[i], sources[i], targets[i]
+        #for i in index:
+        for idx, src, tgt in sorted(zip(indices, sources, targets)):
+            # idx, src, tgt = indices[i], sources[i], targets[i]
             
             # Condition 2. If it does not occur, do nothing.
             if s[idx:idx+len(src)] != src:  
