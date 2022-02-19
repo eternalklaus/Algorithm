68bef379e1cccb5409f1eb3d1c5ab087aaf00ff0
Jiwon <eternalklaus@naver.com>
2022-02-19 09:50:30 +0900

    Time: 104 ms (73.73%), Space: 17.2 MB (33.91%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,33 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # circle detact 
         nextfirst = defaultdict(list)
-        firstnext = defaultdict(list)
         
         for next, first in prerequisites:
             nextfirst[next].append(first)
-            firstnext[first].append(next)
         
-        # find classes don't have any prerequisites
-        queue = []
-        for next in range(numCourses):
-            if not nextfirst.get(next):
-                queue.append(next)
+        visited = [0] * numCourses # visit: 1, visited in current coursework : -1
         
-        attanded = []
-        while queue:
-            first = queue.pop(0)
-            attanded.append(first)
+        def dfs(next):
+            if visited[next] == 1:
+                return True 
+            if visited[next] == -1:
+                return False # circle in current coursework
             
-            nexts = firstnext[first]
-            for next in nexts:
-                nextfirst[next].remove(first)
-                if not nextfirst[next]:
-                    queue.append(next)
-        
-        
-        if len(attanded) == numCourses:
+            firsts = nextfirst[next]
+            visited[next] = -1 # to detect circle
+            for first in firsts:
+                if dfs(first) == False:
+                    return False 
+                
+            visited[next] = 1
             return True 
-        return False
-            
-            
+        
+        for next in range(numCourses):
+            if not dfs(next):
+                return False 
+        return True
+                
             
