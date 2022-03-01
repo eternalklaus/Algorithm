24d2ef415f21513da6349b18d16de9442f1b95e7
Jiwon <eternalklaus@naver.com>
2022-03-01 20:59:42 +0900

    Time: 46 ms (51.34%), Space: 13.8 MB (99.15%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,28 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # if we rob first nums, we should cool down at last session.  
-        # so take two cases, nums, and nums[:-1]
         
-        if len(nums) <= 3: # 1 or 2 or 3
+        def getmaxrob(nums):
+            cooled, justrobbed = 0, nums[0]    
+            for num in nums[1:]: 
+                __cooled = max(cooled, justrobbed)
+                justrobbed = cooled + num
+                cooled = __cooled 
+            return max(cooled, justrobbed)
+        
+        if len(nums) <= 3:
             return max(nums)
-        
-        
-        # robbed first house
-        justrobbed, cooled = nums[0]+nums[2], nums[0]
-        for num in nums[3:-1]:
-            __cooled = max(justrobbed, cooled)
-            justrobbed = cooled + num    
+        return max(getmaxrob(nums[1:]), getmaxrob(nums[:-1]))
             
-            cooled = __cooled 
-        output1 = max(justrobbed, cooled)
-        
-        # didn't robbed first house
-        justrobbed, cooled = nums[1], 0
-        for num in nums[2:]:
-            __cooled = max(cooled, justrobbed)
-            justrobbed = cooled + num
-            
-            cooled = __cooled 
-        output2 = max(justrobbed, cooled)
-        
-        return max(output1, output2)
