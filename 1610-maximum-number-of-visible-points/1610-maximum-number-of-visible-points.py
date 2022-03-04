f2d7c1df41a5baeb668207f53ef5a0afe04e7e03
Jiwon <eternalklaus@naver.com>
2022-03-05 07:35:20 +0900

    Time: 2933 ms (72.79%), Space: 48 MB (51.44%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
@@ -1,28 +1,24 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         import math
-        degrees, count, output = [], 0, 0
         
+        degrees, extra = [], 0, ### extra로 선언한다
+        xx, yy = location
         for x, y in points:
             if [x, y] == location: ### include this point regardless of the degree. 
-                count += 1
+                extra += 1
                 continue 
-            x, y = x-location[0], y-location[1]
-            degree = math.atan2(y, x) / (2 * math.pi) * 360  ### 
-            if degree < 0: 
-                degree += 360 
-            degrees.append(degree)
-        output = max(output, count) ### 아래 for loop가 돌지 않을 것을 대비하여 output을 최초에 업데이트해 둠 
+            degree = math.atan2(y-yy, x-xx) / (2 * math.pi) * 360  ### 
+            degrees.append(degree % 360) ### correct minus value 
         
         degrees.sort()
+        
         degrees += [360+d for d in degrees] ### # 환형 자료구조에서 어떻게 동일한 값은 1번만 포함되도록 할 것인가? 
-        li = 0
+        li = output = 0 ### count 도 사실 필요없지, li, ri가 주어져 있는데. 
         for ri in range(len(degrees)): # ri = enddegree
             while degrees[ri] - degrees[li] > angle: # li = startdegree ###!
                 li += 1
-                count -= 1
-            count += 1
-            output =  max(output, count)
-        return output 
+            output =  max(output, ri - li + 1)
+        return output + extra
             
                 
