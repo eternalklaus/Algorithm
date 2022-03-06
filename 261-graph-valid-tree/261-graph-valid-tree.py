2ffadce28163e6d3cac589e7a0382ba4f3b42a33
Jiwon <eternalklaus@naver.com>
2022-03-07 06:27:36 +0900

    Time: 80 ms (99.71%), Space: 15.3 MB (90.66%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,38 +1,15 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        parent = [i for i in range(n)]
+        def find(x):
+            return x if parent[x] == x else find(parent[x])
         
-        graph = defaultdict(list)
-        
-        ###???
-        if n == 1: return True 
-        
-        for v1, v2 in edges:
-            graph[v1].append(v2)
-            graph[v2].append(v1)
-        
-        # gather leaf
-        queue = []
-        for i in range(n):
-            if i not in graph: return False # i isn't connected at mainstream
-            if len(graph[i]) == 1: 
-                queue.append(i)
-        
-        # drop leaf one by one
-        root = False
-        while queue:
-            leaf = queue.pop(0)
-            nextleaves = graph[leaf]
-            for nl in nextleaves:
-                graph[nl].remove(leaf)
-                if len(graph[nl]) == 0: # we found root! 루트는 하나야 
-                    if root: return False # root is already written
-                    root = nl
-                if len(graph[nl]) == 1:
-                    queue.append(nl)
+        if len(edges) != n-1: return False ### insight
             
-            del graph[leaf] # remove the trace of leaf.. 
-            
+        for e in edges:
+            x, y = find(e[0]), find(e[1]) 
+            if x == y: # 두 엣지가 같은 루트를 가진다면? 
+                return False
+            parent[x] = y
+        return True
         
-        if not graph: # if not all nodes' been toured, return False
-            return True
-        return False 
