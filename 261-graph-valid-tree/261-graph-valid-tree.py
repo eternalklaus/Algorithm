2c72bd85f89be9dd2c7ac027697e25a9cc432d11
Jiwon <eternalklaus@naver.com>
2022-03-07 06:06:59 +0900

    Time: 138 ms (48.25%), Space: 15.5 MB (73.83%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,23 +1,38 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        graph = defaultdict(list)
-        for n1, n2 in edges:
-            graph[n1].append(n2)
-            graph[n2].append(n1)
         
-        # search from arbitrary root(=0)
-        queue, visited = [0], set()
+        graph = defaultdict(list)
+        
+        ###???
+        if n == 1: return True 
+        
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+        
+        # gather leaf
+        queue = []
+        for i in range(n):
+            if i not in graph: return False # i isn't connected at mainstream
+            if len(graph[i]) == 1: 
+                queue.append(i)
+        
+        # drop leaf one by one
+        root = False
         while queue:
-            node = queue.pop(0)
-            if node in visited: return False 
-            visited.add(node)
+            leaf = queue.pop(0)
+            nextleaves = graph[leaf]
+            for nl in nextleaves:
+                graph[nl].remove(leaf)
+                if len(graph[nl]) == 0: # we found root! 루트는 하나야 
+                    if root: return False # root is already written
+                    root = nl
+                if len(graph[nl]) == 1:
+                    queue.append(nl)
             
-            nextnodes = graph[node]
-            # preprocessing
-            for nn in nextnodes: # remove connection to node...
-                graph[nn].remove(node)
-            queue += nextnodes
-            # print (queue)
-                
-        # print ('a')
-        return len(visited) == n
+            del graph[leaf] # remove the trace of leaf.. 
+            
+        
+        if not graph: # if not all nodes' been toured, return False
+            return True
+        return False 
