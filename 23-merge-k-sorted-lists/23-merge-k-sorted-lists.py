f940689b87e7f976baf9f34c621eb64873f357b9
Jiwon <eternalklaus@naver.com>
2022-03-08 12:59:25 -0800

    Time: 3479 ms (9.05%), Space: 18 MB (46.15%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,10 +5,10 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Time complexity: O(n) assuming n is length of list l1
         def merge(l1, l2):
             if not l1 or not l2: return l1 or l2
-            # l1 should always be smaller one 
-            if l1.val > l2.val: l1, l2 = l2, l1 
+            if l1.val > l2.val: l1, l2 = l2, l1 # pick smaller one as l1
             head = l1 
             
             while l1.next and l1.next.val < l2.val:
@@ -18,11 +18,12 @@ class Solution:
             l1.next = l2 
             l2.next = merge(l1next, l2next)
             return head
-
         
+        # time complexity: log(k) given k is total number of linked list
         if not lists: return None
-        head = lists.pop()
-        while lists:
+        
+        while len(lists) >= 2: ### merge 할 때, 2개씩 뽑아야 TC O(logk)임. 1개씩 뽑는다면 O(k)
+            head = lists.pop()
             tail = lists.pop()
-            head = merge(head, tail)
-        return head
+            lists.append(merge(head, tail))
+        return lists[0]
