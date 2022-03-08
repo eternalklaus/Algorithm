34f387ef5c7c45daf9fb6626aff5654ffc0744bd
Jiwon <eternalklaus@naver.com>
2022-03-08 13:40:11 -0800

    Time: 161 ms (49.56%), Space: 17.8 MB (69.80%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,25 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # Time complexity: O(n) assuming n is length of list l1
-        def merge(l1, l2):
-            if not l1 or not l2: return l1 or l2
-            if l1.val > l2.val: l1, l2 = l2, l1 # pick smaller one as l1
-            head = l1 
-            
-            while l1.next and l1.next.val < l2.val:
-                l1 = l1.next 
-            
-            l1next, l2next = l1.next, l2.next 
-            l1.next = l2 
-            l2.next = merge(l1next, l2next)
-            return head
+        import heapq
+        heap = []
+        for i, l in enumerate(lists):
+            if not l: continue 
+            heapq.heappush(heap, (l.val, i))
         
-        # time complexity: log(k) given k is total number of linked list
-        if not lists: return None
+        fakehead = ListNode()
+        node = fakehead 
+        while heap:
+            (v, i) = heapq.heappop(heap)
+            
+            node.next = lists[i] # append popped list (minimum value) into node 
+            node = node.next 
+            
+            if lists[i].next:
+                lists[i] = lists[i].next # move to next list 
+                heapq.heappush(heap, (lists[i].val, i))
         
-        while len(lists) >= 2: ### merge 할 때, 2개씩 뽑아야 TC O(logk)임. 1개씩 뽑는다면 O(k)
-            head = lists.pop()
-            tail = lists.pop()
-            lists.append(merge(head, tail))
-        return lists[0]
+        return fakehead.next 
