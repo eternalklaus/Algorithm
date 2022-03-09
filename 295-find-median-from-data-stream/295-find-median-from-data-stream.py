badfda9440e4ef0682bb738045bc36c0ff3af4c8
Jiwon <eternalklaus@naver.com>
2022-03-08 17:48:22 -0800

    Time: 2196 ms (12.19%), Space: 35.9 MB (70.80%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,27 +1,18 @@
 class MedianFinder:
-    import heapq
+    import bisect
     def __init__(self):
-        # lo -1,-2,-3,-4 // hi 5,6,7,8
-        self.hi_minheap = [] 
-        self.lo_maxheap = [] 
-        
+        self.nums = []
+     
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.hi_minheap, num)
-        minvalue = heapq.heappop(self.hi_minheap)
-        heapq.heappush(self.lo_maxheap, -minvalue)
-        
-        if len(self.lo_maxheap) > len(self.hi_minheap):
-            maxvalue = heapq.heappop(self.lo_maxheap)
-            heapq.heappush(self.hi_minheap, -maxvalue)
-    
-    # O(1)
-    def findMedian(self) -> float:
-        # even 
-        if len(self.hi_minheap) == len(self.lo_maxheap):
-            return (self.hi_minheap[0] - self.lo_maxheap[0]) / 2 
-        else: # odd
-            return self.hi_minheap[0]
+        idx = bisect.bisect(self.nums, num)
+        self.nums.insert(idx, num)
 
+    def findMedian(self) -> float:
+        L = len(self.nums)
+        if len(self.nums) % 2: # 1,2,3 // idx = 1
+            return self.nums[L//2]
+        else: # 1,2,3,4 // idx 1, 2
+            return (self.nums[L//2] + self.nums[L//2-1]) / 2
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
