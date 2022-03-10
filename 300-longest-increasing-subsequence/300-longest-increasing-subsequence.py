2975cb1dad9a8d5cbbf10959ab487c3419b3561b
Jiwon <eternalklaus@naver.com>
2022-03-10 00:14:03 -0800

    Time: 476 ms (70.33%), Space: 14.2 MB (91.15%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,17 +1,13 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         output = []
-        for num in nums: # num = 4
-            idx = -1 # 
-            for i, val in enumerate(output): # output = [1,2,3,6,8]
-                if val >= num: # 신흥강자 ### 같은것도 카운팅해서 idx로 쳐줘야 아래else문으로 안감
-                    idx = i
+        for num in nums:
+            for i, _ in enumerate(output):
+                if output[i] >= num: ### =필수!
+                    output[i] = num
                     break
-            if idx >= 0:
-                output[idx] = num 
-            
-            else: # output = [1,2,3]
+            else: 
                 output.append(num)
-                
+        
+        
         return len(output)
-            
