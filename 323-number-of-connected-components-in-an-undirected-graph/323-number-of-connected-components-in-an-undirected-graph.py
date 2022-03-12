4c8d590ef2d860c567adc8bc6942cbcb35cb995a
Jiwon <eternalklaus@naver.com>
2022-03-12 14:50:55 -0800

    Time: 161 ms (40.84%), Space: 15.5 MB (71.49%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -9,14 +9,14 @@ class Solution:
             parent[x] = find(parent[x])
             return parent[x]
         
+        # 앞 -> 뒤
         for [x, y] in edges:
             px, py = find(x), find(y)
             px, py = min(px, py), max(px, py)
             parent[py] = px 
-            
-        for [x, y] in edges[::-1]:
-            px, py = find(x), find(y)
-            px, py = min(px, py), max(px, py)
-            parent[py] = px 
         
-        return len(set(parent))
+        # 한번 스캔후 find 결과를 리턴해야 함. (parent를 바로 리턴 X)
+        output = set()
+        for p in parent:
+            output.add(find(p))
+        return len(output)
