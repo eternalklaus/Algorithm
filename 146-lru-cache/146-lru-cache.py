0a4e17790276d01465cecbb0f338b9b1d9a4a021
Jiwon <eternalklaus@naver.com>
2022-03-20 14:45:30 -0700

    Time: 924 ms (79.38%), Space: 76.7 MB (8.29%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -2,34 +2,30 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.counter = Counter()
-        self.lrucache = {} # should be less then capacity
+        self.lrucache = {}
         self.accessed = []
+        self.counter = Counter()
 
     def get(self, key: int) -> int:
-        self.accessed.append(key)
-        self.counter[key] += 1
-        # self.lrucache.get(key) ### 0 인 경우 존재.. 
-        # print (self.lrucache, key)
         if key in self.lrucache:
+            self.accessed.append(key)
+            self.counter[key] += 1
             return self.lrucache[key]
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if len(self.lrucache) == self.capacity:
-            # evict key
-            if key not in self.lrucache: # new key would be inserted
+            if key not in self.lrucache: 
+                # evict key
                 while True:
                     k = self.accessed.pop(0)
                     self.counter[k] -= 1
-                    if self.counter[k] == 0 and k in self.lrucache: ### 아무생각 없이 get한 경우 
-                        break 
+                    if self.counter[k] == 0: break 
                 del self.lrucache[k]
-        self.lrucache[key] = value
+        
+        self.lrucache[key] = value 
         self.accessed.append(key)
         self.counter[key] += 1
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
