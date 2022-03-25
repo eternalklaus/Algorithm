8c695c039dadbd52745506dc29a308476b1e79cb
Jiwon <eternalklaus@naver.com>
2022-03-24 23:44:16 -0700

    Time: 983 ms (38.94%), Space: 15.3 MB (49.74%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        L = len(grid)
+        if grid[0][0] == 1: return -1 
+
+        visited = {(0,0):1} # form of (loc1, loc2):3 
+        queue  = [(0,0)]
+
+        while queue:
+            (i, j) = queue.pop(0)
+            
+            for ii in [i-1, i, i+1]:
+                for jj in [j-1, j, j+1]:
+                    if not (0<=ii<L and 0<=jj<L): continue 
+                    if grid[ii][jj] == 1: continue 
+                    if (ii, jj) in visited: continue 
+
+                    visited[(ii, jj)] = visited[(i, j)] + 1 
+                    queue.append((ii, jj)) # new world 
+        
+        if (L-1, L-1) in visited:
+            return visited[(L-1, L-1)]
+        return -1
