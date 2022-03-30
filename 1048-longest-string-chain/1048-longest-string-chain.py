8f9015b5fb9482ffe01967edc77889e5d84e6cc2
Jiwon <eternalklaus@naver.com>
2022-03-30 14:37:53 -0700

    Time: 258 ms (44.73%), Space: 14.4 MB (72.90%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestStrChain(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        # order words by its length!!
+        words.sort(key=lambda x:len(x))
+        # print (words)
+        
+        chainlen, output = {}, 0 
+        for word in words:
+            wordval = 0
+            for i, c in enumerate(word):
+                befword = word[:i] + word[i+1:]
+                befwordval = chainlen.get(befword) or 0
+                
+                wordval = max(wordval, befwordval + 1)
+                
+            chainlen[word] = wordval
+            output = max(output, wordval)
+        
+        print (chainlen)
+        return output 
+        
