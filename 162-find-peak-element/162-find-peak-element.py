da6b39fc4acc5921c1674404a733efc76e5f9cdf
Jiwon <eternalklaus@naver.com>
2022-03-29 22:08:47 -0700

    Time: 32 ms (85.84%), Space: 13.7 MB (43.05%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -1,17 +1,17 @@
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        # binary search 
-        nums.insert(0, float('-inf'))
-        nums.append(float('-inf'))
-        li, ri = 1, len(nums) - 2
+class Solution(object):
+    def findPeakElement(self, nums):
+        nums.insert(0, -float('inf'))
+        nums.append(-float('inf'))
+        li, ri = 1, len(nums)-2 
         while li < ri:
-            mi = (li + ri) // 2
-            if nums[mi-1] < nums[mi] and nums[mi+1] < nums[mi]:
-                return mi - 1 
-            
+            mi = (li + ri)//2
+            if nums[mi] > nums[mi-1] and nums[mi] > nums[mi+1]:
+                return mi - 1
             if nums[mi-1] > nums[mi]: # pick left side 
-                ri = mi-1
-            else: # nums[mi-1] < nums[mi] -> pick right and middle side 
-                li = mi+1
-        
+                ri = mi - 1
+                continue 
+            if nums[mi+1] > nums[mi]: # pick right side 
+                li = mi + 1
+                
         return li - 1
+        
