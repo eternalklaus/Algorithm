109f6bfa3468d0cb5aab9a522a701942c55754fc
Jiwon <eternalklaus@naver.com>
2022-04-01 14:12:42 -0700

    Time: 144 ms (12.73%), Space: 14.5 MB (6.49%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,17 +1,12 @@
 class Solution:
-    import math 
     def evalRPN(self, tokens: List[str]) -> int:
-        # push numbers into stack, and if we encounter operator, pop two numbers
         stack = []
         for token in tokens:
-            if token.isnumeric() or token[1:].isnumeric(): # token.startswith('-'): 
-                stack.append(token) 
+            if token.isnumeric() or token[1:].isnumeric(): ### - value isn't considered as numeric
+                stack.append(token)
             else:
                 num2 = stack.pop()
                 num1 = stack.pop()
-                result = eval(f'{num1} {token} {num2}')
-                if result % 1:
-                    if result > 0: result = math.floor(result)
-                    else: result = math.ceil(result)
-                stack.append(result)
-        return int(stack[0])
+                stack.append(int(eval(f'{num1} {token} {num2}'))) ### // 보다 더 심오한 무언가가 있음 
+        return stack[0]
+                
