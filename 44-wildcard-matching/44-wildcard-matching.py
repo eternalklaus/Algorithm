038729a8cf69cff6822d30cfd70d58a358f09799
Jiwon <eternalklaus@naver.com>
2022-04-03 23:06:20 -0700

    Time: 2421 ms (5.01%), Space: 112.5 MB (21.83%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,30 +1,23 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        # ?: single char
-        # *: sequance chars 
-        # ab*def abcdef abddd def ddd def
-        # if some chars appears after *, 
         S, P = len(s), len(p)
         
         @cache 
-        def ismatch(si, pi): ### what if s is empty string?
+        def check(si, pi):
             # print (s[si:], p[pi:])
             if si >= S and pi >= P:
-                return True
-            if pi >= P: # pattern is exhausted even though string still remained
+                return True 
+            if pi >= P: # pattern is all consumed ... 
                 return False 
-            if si >= S: # '', "***"
-                if p[pi] == '*':
-                    return ismatch(si, pi+1) 
-                else: 
-                    return False 
-
-            if s[si] == p[pi] or p[pi] == '?':
-                return ismatch(si+1, pi+1)
-            if p[pi] == '*':
-                for ssi in range(si, S+1): # from si to S ... 
-                    if ismatch(ssi, pi+1): return True 
-                
-            return False 
+            if si >= S: # string is all consumed ... 
+                return set(p[pi:]) == {'*'} # give one more chance...
             
-        return ismatch(0, 0)
+            if p[pi] == '?' or p[pi] == s[si]:
+                return check(si+1, pi+1)
+            
+            if p[pi] == '*':
+                for sii in range(si, S+1):
+                    if check(sii, pi+1): return True 
+            return False 
+
+        return check(0,0)
