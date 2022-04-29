131d03b5ff57c5a1898a6457f71a085a9c01c5fd
Jiwon <eternalklaus@naver.com>
2022-04-28 18:10:25 -0700

    Time: 561 ms (63.22%), Space: 20.9 MB (75.59%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,18 +1,17 @@
-# [1,4,5,6] -> 0:0 1:1 ### bisect = "equal or bigger
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people = [-p for p in people]
-        people.sort()
+        people.sort(reverse=True)
         output = 0
-        while people: # [-4, -3, -2, -1]
-            p1_val = people.pop(0)
-            p2_val = limit + p1_val 
-            p2_idx = bisect.bisect(people, -p2_val-1) # bigger value!!! if finding 1 and [1], returns idx 1. 
-            # print (people)
-            # print (p1_val, p2_val, p2_idx)
-            if p2_idx < len(people):
-                people.pop(p2_idx)
-            
+
+        # two pointer 
+        li, ri = 0, len(people)-1
+        while li <= ri:
+            capacity = limit 
+            # increase li 
+            capacity -= people[li]
+            li += 1
+            # decrease ri 
+            if people[ri] <= capacity:
+                ri -= 1
             output += 1
-                
         return output 
