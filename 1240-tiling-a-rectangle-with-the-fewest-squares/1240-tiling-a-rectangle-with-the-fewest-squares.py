7215c45cfdc438064a7b067b0297ad259e6b3fb9
Jiwon <eternalklaus@naver.com>
2022-04-29 16:31:11 -0700

    Time: 851 ms (13.36%), Space: 13.9 MB (73.00%) - LeetHub

--- 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.py
+++ 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.py
@@ -1,33 +1,32 @@
 class Solution:
     def tilingRectangle(self, n: int, m: int) -> int:
+        emptyheight = [n] * m
+        self.output = n*m
         
-        def getminbox(heights):
-            maxheight = max(heights)
-            li = heights.index(maxheight)
-            ri = li + 1 
-            while ri < m and heights[ri] == maxheight:
-                ri += 1 
-            return maxheight, (ri-li), li
-
-        # rule 1. 무조건 ceil부터 채운다. 따라서 heights는 아래에서부터 채울수있는 높이다
-       
-        self.output = n * m
-
-        def fillit(heights, cnt): ### heights = empty spaces height  
-            if any(heights) == False: # all is 0
+        def fillit(eh, cnt): # empty height 
+            if cnt > self.output: 
+                return
+            if any(eh) == False: ###!!!
                 self.output = min(self.output, cnt)
                 return 
-            if cnt > self.output:
-                return 
-            height, width, li = getminbox(heights)
-            boxsize = min(height, width)
-            # fill every sized boxes
-            # for size in range(1, boxsize+1): ###!!! 아... 가장 큰것부터 채워주는 센스... 
-            for size in range(boxsize, 0, -1):
-                newheights = heights.copy()
-                for i in range(li, li + size):
-                    newheights[i] -= size 
-                fillit(newheights, cnt+1)
+            
+            # get maximum height that can e filled 
+            maxh = max(eh)
+            li = eh.index(maxh)
+            ri = li + 1
+            while ri < m and eh[ri] == maxh:
+                ri += 1
+            
+            size = min(maxh, ri-li)#!!!
+            
+            # fill sized box
+            for sz in range(size, 0, -1):
+                eh_copy = deepcopy(eh)
+                for i in range(li, li+sz):
+                    eh_copy[i] -= sz
+                fillit(eh_copy, cnt+1)
         
-        fillit([n] * m, 0)
+        fillit(emptyheight, 0)
         return self.output 
+            
+                
