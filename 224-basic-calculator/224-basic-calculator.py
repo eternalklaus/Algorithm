2e180f8a30a1d66a5d9df0447fd290707c7f8695
Jiwon <eternalklaus@naver.com>
2022-05-07 09:46:48 +0900

    Time: 130 ms (49.85%), Space: 15.4 MB (51.78%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,46 +1,43 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # push it simply, pop it calculation
-        def getval(stack):
-            # pop stack until '(' appears 
-            output, val = 0, 0
+        s = s.replace(' ','')
+        i, stack, L = 0, [], len(s)
+        
+        def stackcalc():
+            nonlocal stack 
+            preval, output = '0', 0
+            
             while stack:
                 x = stack.pop()
-                if x == '(': break
+                if x == '(': break 
                     
-                if x.isnumeric() or x[1:].isnumeric():
-                    val = int(x)
+                if x == '+':
+                    output += int(preval)
+                    preval = '0'
                 elif x == '-':
-                    output -= val 
-                    val = 0
-                elif x == '+':
-                    output += val 
-                    val = 0
-                    
-            return output + val
+                    output -= int(preval)
+                    preval = '0'
+                else: # numeric value 
+                    preval = x 
+            output += int(preval)
+            return str(output)
         
-        stack = []
-        i = 0
-        while i < len(s):
-            if s[i] == ')':
-                val = getval(stack)
-                
-                stack.append(str(val))
+        while i<L:
+            # print (stack)
+            if s[i] == ')': 
+                val = stackcalc() 
+                stack.append(val)
                 i += 1
-                continue 
-            
+                
             elif s[i].isnumeric():
-                ri = i
-                while ri < len(s) and s[ri].isnumeric():
-                    ri += 1
+                ri = i+1
+                while ri < L and s[ri].isnumeric():
+                    ri += 1 
                 stack.append(s[i:ri])
                 i = ri
-                continue 
             
-            else:
-                stack.append(s[i]) # if +, -, (
+            else: # (, +, -
+                stack.append(s[i])
                 i += 1
-                continue
-                
-        print (stack)
-        return getval(stack)
+        
+        return stackcalc()
