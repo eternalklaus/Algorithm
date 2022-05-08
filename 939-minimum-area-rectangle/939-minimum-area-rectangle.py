158ac7b50675b5e3a40cec7af8cc4cf5f5dd156d
Jiwon <eternalklaus@naver.com>
2022-05-08 10:56:12 +0900

    Time: 2510 ms (11.80%), Space: 14.1 MB (86.66%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.py
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.py
@@ -1,27 +1,22 @@
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
-        points.sort()
+        points.sort() # x would be acsending order 
+        INF = float('inf')
+        
         xy = defaultdict(list)
         for x,y in points:
             xy[x].append(y)
         
-        output = float('inf')
-        for x2 in xy:
+        output = INF
+        for x2 in xy: ###
             for x1 in xy:
-                if x1 >= x2: break  ### 순서를 바꿔서 브래이크 제동을 걸어줬다
-                if x2-x1 > output: continue 
-                ys = [y for y in xy[x1] if y in xy[x2]]
-                if len(ys) <= 1: continue
-                
-                # get most mininmum diff between all element 
-                ys.sort()
-                dy = float('inf')
-                # print (x1,x2)
-                # print (ys)
-                for y1, y2 in zip(ys, ys[1:]):
+                if x2 <= x1: break
+                ycommon = [y for y in xy[x1] if y in xy[x2]] # 공통 y만 구한다
+                ycommon.sort()
+                dy = INF
+                for y1, y2 in zip(ycommon, ycommon[1:]):
                     dy = min(dy, y2-y1)
-                    # print (f'dy={dy}')
-                output = min(output, (x2-x1)*dy)
-                # print (f'output={output}')
-        return output if output != float('inf') else 0
-                
+                output = min((x2-x1) * dy, output)
+        
+        if output < INF: return output 
+        return 0
