181db6cf8770ea492ff60d72e9757e6a8117e318
Jiwon <eternalklaus@naver.com>
2022-05-23 19:49:52 +0900

    Time: 1958 ms (32.56%), Space: 14.2 MB (86.08%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.py
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.py
@@ -1,23 +1,28 @@
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
-        points.sort() # x would be acsending order 
-        INF = float('inf')
-        
         xy = defaultdict(list)
-        for x,y in points:
+        for x, y in points:
             xy[x].append(y)
         
-        output = INF
-        for x2 in xy: ###
-            for x1 in xy:
-                if x2 <= x1: break
-                if x2-x1 > output: continue ### heuristic optimization
-                ycommon = [y for y in xy[x1] if y in xy[x2]] # 공통 y만 구한다
-                ycommon.sort()
-                dy = INF
-                for y1, y2 in zip(ycommon, ycommon[1:]):
-                    dy = min(dy, y2-y1)
-                output = min((x2-x1) * dy, output)
+        output = float('inf')
         
-        if output < INF: return output 
-        return 0
+        xlist = sorted(xy)
+        for ri in range(len(xlist)):
+            for li in range(ri-1, -1, -1):
+                x1, x2 = xlist[li], xlist[ri]
+                dx = x2 - x1
+                if dx >= output: # 가로길이에서 이미 넘어버림
+                    break 
+                    
+                # filter common y
+                common_y = [y for y in xy[x1] if y in xy[x2]]
+                common_y.sort() 
+                
+                # pick minimum dy  
+                dy = float('inf')
+                for y1, y2 in zip(common_y, common_y[1:]):
+                    dy = min(y2-y1, dy)
+                output = min(output, dy*dx)
+        return 0 if output == float('inf') else output
+                
+                
