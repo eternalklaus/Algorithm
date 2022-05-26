2c158d32a29f0dab646eed77ce2137e4da9d2be1
Jiwon <eternalklaus@naver.com>
2022-05-27 07:14:18 +0900

    Time: 6786 ms (19.52%), Space: 236 MB (5.18%) - LeetHub

---
+++ 464-can-i-win/464-can-i-win.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        remainder = [(maxChoosableInteger-i) for i in range(maxChoosableInteger)]
+        
+        @cache
+        def can_win(total, remainder):
+            # print (total, remainder)
+            if total >= desiredTotal:
+                return False # 이미 이전에 조건을 충족해버림..
+            
+            for num in remainder:
+                if can_win(total + num, tuple([n for n in remainder if n != num])) == False: # 상대가 진다면?
+                    return True
+            return False 
+        
+        if desiredTotal == 0:
+            return True 
+        if sum(remainder) < desiredTotal:
+            return False 
+        print (1)
+        return can_win(0, tuple(remainder))
