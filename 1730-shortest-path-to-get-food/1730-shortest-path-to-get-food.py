b3342361e830764b2434ff2bd7b0e06e1c2190e9
Jiwon <eternalklaus@naver.com>
2022-05-28 12:43:10 +0900

    Time: 1136 ms (5.13%), Space: 15.1 MB (77.50%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -1,22 +1,20 @@
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
-        C, R = len(grid), len(grid[0])
-        (i, j) = [[i, j] for i in range(C) for j in range(R) if grid[i][j] == '*'][0] ###<-
-        outputs = []
-        
+        I, J = len(grid), len(grid[0])
+        i, j = [(i, j) for i in range(I) for j in range(J) if grid[i][j] == '*'][0]
         queue = [(0, i, j)]
-        while queue:
-            steps, i, j = heappop(queue)
-            if not 0<=i<C or not 0<=j<R or grid[i][j] == 'X': 
-                continue 
-                
-            if grid[i][j] == '#':
-                return steps
-                
-            grid[i][j] = 'X' # mark visited location(this is cheapest way to reach here)
-            heappush(queue, (steps+1, i+1, j))
-            heappush(queue, (steps+1, i-1, j))
-            heappush(queue, (steps+1, i, j+1))
-            heappush(queue, (steps+1, i, j-1))
         
+        while queue:
+            weight, i, j = heapq.heappop(queue)
+            # print (weight, i, j)
+            for ii, jj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                if not (0<=ii<I and 0<=jj<J):
+                    continue 
+                if grid[ii][jj] == 'X':
+                    continue 
+                if grid[ii][jj] == '#': 
+                    return weight + 1
+                if grid[ii][jj] == 'O': 
+                    grid[ii][jj] = 'X' # visited mark
+                    heapq.heappush(queue, (weight + 1, ii, jj))
         return -1
