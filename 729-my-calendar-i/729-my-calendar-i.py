3f47303b6bc54bf43b1166be8af3d5201d90e182
Jiwon <eternalklaus@naver.com>
2022-06-19 22:36:13 -0700

    Time: 295 ms (80.03%), Space: 14.8 MB (57.98%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,21 +1,23 @@
+MAX = 10**9+1
 class MyCalendar:
     import bisect 
     def __init__(self):
-        self.event_start = [0, 10**9+1] # consist of (start, end)
-        self.event_end = [0, 10**9+1]
+        self.starts = [-1, MAX]
+        self.ends = [-1, MAX]
 
     def book(self, start: int, end: int) -> bool:
-        i = bisect.bisect(self.event_start, start) # 'i' is later event then 'start'
-        
-        prev_end = self.event_end[i-1]
-        next_start = self.event_start[i]
-        if prev_end <= start:
-            if next_start >= end:
-                self.event_start.insert(i, start)
-                self.event_end.insert(i, end)
-                return True 
+        # 근데 bisect 는 한번에 하나밖에 못찾아..!
+        idx = bisect.bisect(self.starts, start)
+
+        next_start = self.starts[idx]
+        before_end = self.ends[idx-1]
+
+        if before_end <= start and end <= next_start:
+            self.starts.insert(idx, start)
+            self.ends.insert(idx, end)
+            return True 
         return False 
-            
+        
 
 
 # Your MyCalendar object will be instantiated and called as such:
