66a15aaea0d14b66bd1d571a34edc0fca1acb46c
Jiwon <eternalklaus@naver.com>
2023-04-01 17:33:17 +0900

    Time: 65 ms (41.91%), Space: 14.2 MB (26.45%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,33 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        # chars per pair = numRows * 2 - 2
-        # every pair[0] in the same row
-        # every pair[1], pair[-1] in the same row
-        # every pair[2], pair[-2] in the same row
-        # ... 
-        # every pair[numRows - 1] in the same row
+        from collections import defaultdict
         
-        if numRows == 1:
-            return s
-
-        rowLen = numRows * 2 - 2
-        L, output = len(s), ''
-        
-        for i in range(numRows):
-            if i == 0 or i == numRows - 1:
-                row = s[i::rowLen]
-                output += ''.join(row)
-                
-            else:
-                li = i
-                ri = rowLen - i
-                row1 = s[li::rowLen] + '_'
-                row2 = s[ri::rowLen] + '_'
-                row = [c1 + c2 for c1, c2 in zip(row1, row2)]
-                
-                output += ''.join(row).rstrip('_')
-        
-        return output
+        rows = defaultdict(list)
+        pivot, direction = 0, -1 # 1 means go to downward
+        for c in s:
+            rows[pivot].append(c)
+            if pivot in [0, numRows-1]: 
+                direction *= (-1)
             
-            
+            pivot += direction
         
+        output = ''
+        for i, row in rows.items():
+            output += ''.join(row)
+        return output 
+            
