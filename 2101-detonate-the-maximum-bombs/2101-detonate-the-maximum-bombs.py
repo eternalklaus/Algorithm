8e29e24dacfad5d95a9e089bd9bc21c7f7174bba
Jiwon <eternalklaus@naver.com>
2023-04-18 12:44:19 +0900

    Time: 2675 ms (12.72%), Space: 15 MB (14.45%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,35 +1,44 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        # 1. build the graph
-        # 2. dfs it 
-        
-        G = defaultdict(list)
+        from functools import lru_cache
         L = len(bombs)
         
-        def connected(i, j):
-            x1, y1, r1 = bombs[i]
-            x2, y2, r2 = bombs[j]
-            if (x1-x2)**2 + (y1-y2)**2 <= r1**2:
-                return True
-            return False
+        # directed graph
+        G = defaultdict(list)
         
+        
+        def connected(i, j):
+            x1,y1,r1 = bombs[i]
+            x2,y2,r2 = bombs[j]
+            if (x1-x2)**2 + (y1-y2)**2 <= r1**2: ###
+                return True
+            return False 
+            
+            
+        L = len(bombs)
         for i in range(L):
             for j in range(L):
                 if i == j: continue 
-                if connected(i, j): G[i].append(j)
-                if connected(j, i): G[j].append(i)
+                if connected(i, j): 
+                    G[i].append(j)
+                if connected(j, i):
+                    G[j].append(i)
         
-        
-        def dfs(i, exploded):
-            exploded.add(i)
-            for nexti in G[i]:
-                if nexti in exploded: continue 
-                dfs(nexti, exploded)
+        def visit(i, visited):
+            if i in visited: return 0
+            
+            visited.add(i)
+            for j in G[i]:
+                visit(j, visited)
                 
             
+        
         output = 0
-        for i in range(L):
-            exploded = set()
-            dfs(i, exploded)
-            output = max(output, len(exploded))
+        for i in range(L): # the visiting order matters... 
+            visited = set()
+            visit(i, visited)
+            output = max(output, len(visited))
         return output 
+            
+                
+                
