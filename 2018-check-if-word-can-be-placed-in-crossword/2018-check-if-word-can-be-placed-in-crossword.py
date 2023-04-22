6b9d6172465fa5577941ea6d01a8cd7e6e1c510f
Jiwon <eternalklaus@naver.com>
2023-04-22 16:25:16 +0900

    Time: 1776 ms (16.52%), Space: 26.8 MB (59.13%) - LeetHub

--- 2018-check-if-word-can-be-placed-in-crossword/2018-check-if-word-can-be-placed-in-crossword.py
+++ 2018-check-if-word-can-be-placed-in-crossword/2018-check-if-word-can-be-placed-in-crossword.py
@@ -1,54 +1,19 @@
 class Solution:
     def placeWordInCrossword(self, board: List[List[str]], word: str) -> bool:
-        I, J = len(board), len(board[0])
-        
-        def compare(candidate, word):
-            # print ('-'+candidate+'-', word)
-            if len(candidate) != len(word): return False
-            result1, result2 = True, True
-            
-            for c1, c2 in zip(candidate, word):
-                if c1 == c2: continue 
-                if c1 == ' ': continue
-                else: 
-                    result1 = False
-                    break
-            
-            for c1, c2 in zip(candidate, word[::-1]):
-                if c1 == c2: continue 
-                if c1 == ' ': continue 
-                else: 
-                    result2 = False
-                    break
-            
-            return result1 or result2
-            
-        candidate = ''
-        # left to right check
-        for i in range(I):
-            for j in range(J):
-                c = board[i][j]
-                if c == '#':
-                    if compare(candidate, word): return True
-                    candidate = ''
-                    continue 
-                candidate += c
-                if j == J-1:
-                    if compare(candidate, word): return True
-                    candidate = ''
-                    continue 
-        
-        candidate = ''
-        # top to bottom check
-        for j in range(J):
-            for i in range(I):
-                c = board[i][j]
-                if c == '#':
-                    if compare(candidate, word): return True
-                    candidate = ''
+        def match(word, w):
+            if len(word) != len(w): return False 
+            for c1,c2 in zip(word, w):
+                if c1 == c2 or c2 == ' ': # exactly match or wildcard match
                     continue
-                candidate += c
-                if i == I-1:
-                    if compare(candidate, word): return True
-                    candidate = ''
-                    continue 
+                return False
+            return True
+            
+        def compare(board):
+            for w in board:
+                w = ''.join(w)
+                words = w.split('#')
+                for w in words:
+                    if match(word, w): return True
+                    if match(word[::-1], w): return True
+        
+        return compare(board) or compare(zip(*board))
