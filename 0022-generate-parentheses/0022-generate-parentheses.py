d567010e5cffcfe50091dc39f28bc40105fbee28
Jiwon <eternalklaus@naver.com>
2023-04-23 10:16:47 +0900

    Time: 36 ms (58.13%), Space: 14.1 MB (96.04%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,28 +1,26 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # size = n 인곳의 push-pop 조합 
+        # backtracking 
         
-        # state: ( -> 1, (( -> 2, () -> 0
-        # usedpush: (()( -> 3. so it used all of the push life!
         output = []
         
-        # @cache
-        def dfs(line, state, usedpush): 
+        def backtracking(line, pushcnt, popcnt):
             if len(line) == 2*n:
                 output.append(line)
+                return 
+            
+            if pushcnt == n:
+                backtracking(line + ')', pushcnt, popcnt+1)
                 return
             
-            if usedpush == n: # only choice(close) - consumed all the opportunity of pushes../
-                dfs(line+')', state-1, usedpush)
+            if pushcnt == popcnt:
+                backtracking(line + '(', pushcnt+1, popcnt)
                 return
             
-            if state == 0: # only choice(open)
-                dfs(line+'(', state+1, usedpush+1)
-                return
-            
-            dfs(line+')', state-1, usedpush)
-            dfs(line+'(', state+1, usedpush+1)
+            backtracking(line + ')', pushcnt, popcnt+1)
+            backtracking(line + '(', pushcnt+1, popcnt)
+            return
         
-        dfs('', 0, 0)
-        return output
-                
+        backtracking('', 0, 0)
+        return output 
+            
