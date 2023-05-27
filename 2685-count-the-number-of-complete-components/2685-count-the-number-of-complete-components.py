28a47d1c57127a8d7953e006a28d98ea8618daa3
Jiwon <eternalklaus@naver.com>
2023-05-27 21:25:00 +0900

    Time: 882 ms (7.93%), Space: 17 MB (38.75%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,52 +1,44 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        connected = defaultdict(list)
-        groupking = [i for i in range(n)]
+        # 내가 생각하던 알고리즘에 문제가 있음을 깨달았다. 
+        # 거바 여기서도 한바쿠 돌자너~ https://chiefcoder.tistory.com/55
         
-        def union(i, j):
-            # root를 찾기 위한 find
-            root = min(find(i), find(j))
-            groupking[i] = root
-            groupking[j] = root
-            # 업데이트 싸악-을 위한 find
-            find(i), find(j)
+        # union find
+        roots = [i for i in range(n)]
         
         def find(i):
-            if groupking[i] == i:
+            if roots[i] == i: 
                 return i
-            root = find(groupking[i])
-            # 여기서 찾아주면서 싸악- 새 신을 섬기는거지
-            groupking[i] = root
-            return root
+            output = find(roots[i])
+            roots[i] = output
+            return output
         
-        # union-find
+        def union(i, j):
+            iroot, jroot = find(i), find(j)
+            iroot, jroot = min(iroot,jroot), max(iroot,jroot)
+            roots[jroot] = iroot
+            find(i), find(j) # 싸악- 업데이트
+        
         for i, j in edges:
-            connected[i].append(j)
-            connected[j].append(i)
-            connected[i].append(i)
             union(i, j)
         
-        def identical(a, b):
-            if set(a) == set(b): return True
-            return False
-        
-        
-        # check whether among all connected nodes has connection with each other
-        groups = defaultdict(list)
+        ### 이거 꼭 해줘야 함!!!
         for i in range(n):
-            groups[groupking[i]].append(i)
+            find(i)
+        
+        # grouping
+        group = defaultdict(list)
+        for i, root in enumerate(roots):
+            group[root].append(i)
+        
+        graph = defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
         
         output = 0
-        for groupking, members in groups.items():
-            members.sort()
-            is_thisgroup_perfact = 1
-            for i in members:
-                # _ = connected[i] + [i] # 왜 이렇게하니까 connected가 바뀌는거같지...?
-                
-                if identical(members, connected[i] + [i]):
-                    continue
-                else: 
-                    is_thisgroup_perfact = 0
-            output += is_thisgroup_perfact
+        for root, member in group.items():
+            if all([len(graph[m])==len(member)-1 for m in member]):
+                output += 1
         return output
             
