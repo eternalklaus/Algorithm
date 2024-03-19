5b000ee73072d9c6e91bcad71347d2267ef4b8b2
Jiwon <eternalklaus@naver.com>
2024-03-19 20:12:00 +0900

    Time: 2803 ms (11.7%), Space: 99.1 MB (30.85%) - LeetHub

--- 0805-split-array-with-same-average/0805-split-array-with-same-average.py
+++ 0805-split-array-with-same-average/0805-split-array-with-same-average.py
@@ -15,7 +15,7 @@ class Solution:
         for i in range(L):
             # nums[i]까지 왔다? 그럼 조합의 최대길이는 i+1
             # for j in range(i,-1,-1): # sums[3]의 모든것들에 num을더해 sums[4]로 보낸다. 2를3으로 보낸다. 1을 2로보낸다.
-            for j in range(min(i, L//2), -1, -1):
+            for j in range(min(i, L), -1, -1):
                 for befsum in sums[j]:
                     sums[j+1].add(befsum+nums[i])
                     if j+1 != L and fit(befsum+nums[i], j+1): return True
