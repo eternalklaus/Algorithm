9b22bfcedd92e20b5c1a9a3523bd448d7034074b
Jiwon <eternalklaus@naver.com>
2024-05-21 19:16:53 +0900

    Time: 31 ms (99.1%), Space: 16.6 MB (70.27%) - LeetHub

--- 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.py
+++ 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.py
@@ -1,64 +1,59 @@
 class Solution:
     def maxStudents(self, seats: List[List[str]]) -> int:
-        R, C = len(seats), len(seats[0])
-        
+        I, J = len(seats), len(seats[0])
+        source, sink = (-1,-1), (I,J)
         graph = defaultdict(dict)
         total_seats = 0
-        source = (-1, -1)
-        sink = (R, C)
         
-		# seats at even row are on the source side
-		# create forward edge as well as backward edge (residual edge) with all neighbours (including odd-row seats and source)
-        for r in range(R):
-            for c in range(0, C, 2):
-                if seats[r][c] == '#': continue
+        # 0,2,4.. = source node
+        for i in range(I):
+            for j in range(0,J,2):
+                if seats[i][j]=='#':continue
+                n1 = (i,j)
                 total_seats += 1
-                graph[source][r, c] = 1
-                graph[r, c][source] = 0
-                for nr, nc in [(r, c-1), (r-1, c-1), (r-1, c+1), (r, c+1), (r+1, c+1), (r+1, c-1)]:
-                    if 0 <= nr < R and 0 <= nc < C and seats[nr][nc] == '.':
-                        graph[r, c][nr, nc] = 1
-                        graph[nr, nc][r, c] = 0
+                graph[source][n1] = 1
+                graph[n1][source] = 0
+                for n2 in [(i,j-1), (i-1,j-1), (i-1,j+1), (i,j+1), (i+1,j+1), (i+1,j-1)]: # 인접해서 앉을 수 없는 경로들 
+                    ni,nj = n2
+                    if 0<=ni<I and 0<=nj<J and seats[ni][nj]=='.':
+                        graph[n1][n2] = 1
+                        graph[n2][n1] = 0
         
-		# create edges for the odd-row seats with sink
-        for r in range(R):
-            for c in range(1, C, 2):
-                if seats[r][c] == '#': continue
+        # 1,3,5.. = sink node
+        for i in range(I):
+            for j in range(1,J,2):
+                if seats[i][j] == '#': continue
+                n1 = (i,j)
                 total_seats += 1
-                graph[r, c][sink] = 1
-                graph[sink][r, c] = 0
-    
-		# bfs to find a path from source to sink. if found, augmenting the edges along the path
-		# here all the edge capacity is max 1, therefore, once a path is found, we can include max network flow by 1
-        def bfs(node, graph):
-            visited = {node}
-            queue = deque([node])
-            parent = {node: None}
-            
+                graph[n1][sink] = 1
+                graph[sink][n1] = 0
+        
+        # bfs로 source -> sink 경로를 탐색한다
+        def bfs():
+            queue, visited = deque([source]), {source}
             found = False
+            parent = {source:None} ### 역사를 기록한다. 찾은 경로의 유량을 감소시켜 주기 위해서!
             while queue:
                 curr = queue.popleft()
-                if curr == (R, C):
+                if curr == sink:
                     found = True
                     break
                 for nei in graph[curr]:
                     if nei in visited or graph[curr][nei] == 0: continue
-                    visited.add(nei)
                     queue.append(nei)
+                    visited.add(nei)
                     parent[nei] = curr
-            
             if not found: return False
-            curr = (R, C)
+
+            curr = sink # 자 이제 유량을 감소시켜볼까
             prev = parent[curr]
-            while prev:               # this is the augmenting part, decrease forward edge by 1, and increase backwared edge by 1
+            while curr != source:
                 graph[prev][curr] -= 1
                 graph[curr][prev] += 1
-                curr, prev = prev, parent[prev]
+                prev, curr = parent[prev], parent[curr]
             return True
         
-		# max flow equals min cut, equals maximum bipartite matching, meaning at least this amount of seats can't be used.
         min_cut = 0
-        while bfs(source, graph):
+        while bfs():
             min_cut += 1
-
         return total_seats - min_cut
