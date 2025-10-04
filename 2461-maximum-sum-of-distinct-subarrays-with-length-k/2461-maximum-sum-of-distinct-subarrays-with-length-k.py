ce37cf4ce727dec0a95e812692f7ca0371cb275d
Jiwon <eternalklaus@naver.com>
2025-10-04 13:14:51 +0900

    Time: 148 ms (31.09%), Space: 37.6 MB (7.62%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,33 +1,32 @@
 class Solution:
-    from collections import Counter
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        counter = Counter()
-        left = 0
-        right = -1
-        count_of_one = 0
-        currentsum = 0
-        maximumsum = 0
+        # left 이동 케이스: len > k, nums[i] exists in sets
+        # right 이동 케이스: always
+        left, right = 0, -1
+        output = 0
+        window = {}
+        windowsum = 0
         while True:
-            # right - left + 1 should be k
             right += 1
-            if right == len(nums): break
-            counter[nums[right]] += 1
-            if counter[nums[right]] == 1: count_of_one += 1
-            if counter[nums[right]] == 2: count_of_one -= 1
-            currentsum += nums[right]
-
-            if right - left + 1 < k: 
-                continue # 아직 비교할 단계가 아니고, 슬라이딩윈도우가 커져가고 있는 단계
-            if right - left + 1 > k: # 윈도우 사이즈가 초과됨
-                counter[nums[left]] -= 1
-                if counter[nums[left]] == 1: count_of_one += 1
-                if counter[nums[left]] == 0: count_of_one -= 1
-                currentsum -= nums[left]
-                left += 1
+            if right >= len(nums): break
             
-            # print (nums[left:right+1], count_of_one, currentsum)
-            if count_of_one == k: 
-                maximumsum = max(maximumsum, currentsum)
-
-        return maximumsum
+            if nums[right] in window: 
+                idx = window[nums[right]]
+                left = idx + 1
+                right = idx
+                window = {} # 윈도우 호핑 초기화
+                windowsum = 0
             
+            else:
+                while right - left + 1 > k:
+                    del window[nums[left]]
+                    windowsum -= nums[left]
+                    left += 1
+                window[nums[right]] = right
+                windowsum += nums[right]
+            
+            if len(window) == k:
+                output = max(windowsum, output)
+        return output
+
+
