9e4664a9f74e1ff3a1a9b1740d76d5f4a597f24c
Jiwon <eternalklaus@naver.com>
2025-10-06 15:42:20 +0900

    Time: 203 ms (36.2%), Space: 18.5 MB (15.79%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,29 +1,23 @@
 class Solution:
-    from itertools import permutations
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # 가능한 p의 모든 케이스들을 set에 저장 
-        # s를 돌면서 모든 윈도우에 대해 if window in set: output.append(i) 수행
-
-        l = []
+        pcounter = Counter()
+        scounter = Counter()
         output = []
 
-        for c in p: l.append(c)
-        perm = set()
-        for perm_str in permutations(l):
-            perm.add(''.join(perm_str))
-
+        for c in p:
+            pcounter[c] += 1
         left = 0
         right = -1
-        while True: 
+        
+        while True:
             right += 1
-            if right >= len(s): break 
-            if right - left + 1 > len(p):
+            if right >= len(s): break
+            
+            scounter[s[right]] += 1
+            while right - left + 1 > len(p):
+                scounter[s[left]] -= 1
                 left += 1
             if right - left + 1 == len(p):
-                curr_str = ''.join(s[left:right+1])
-                if curr_str in perm: 
-                    output.append(left)
+                if pcounter == scounter: output.append(left)
         
         return output
-
-        
