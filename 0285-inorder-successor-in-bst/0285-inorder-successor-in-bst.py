1da6080a7df620af7c40c9726f1ce4bdac0c9a2f
Jiwon <eternalklaus@naver.com>
2025-10-31 09:08:50 +0900

    Time: 53 ms (77.32%), Space: 20.9 MB (76.46%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -7,16 +7,13 @@
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
-        heap = []
-        def traverse(node):
-            nonlocal heap
-            if node == None: return 
+        # 이전에 나왔던 확실히 안전한 조상을 따로 저장한다. 왜? 현재 노드의 값을 현재 phase에 알 수 없으니까..현재페이즈에서 커버하긴 머리 아프니까..!
+        succ = None
+        node = root
+        while node:
             if node.val > p.val:
-                heapq.heappush(heap, (node.val, node))
-            traverse(node.left)
-            traverse(node.right)
-        traverse(root)
-
-        if not heap: return None
-        (_, node) = heappop(heap)
-        return node
+                succ = node
+                node = node.left
+            else:
+                node = node.right
+        return succ
